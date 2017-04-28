c4f15f6d867f71cc21b292705173e9ed18aaa5bd
Joel Abrahams <abrahamsjo@gmail.com>
2017-04-28 16:54:05 +0200

    Add Graph and Menu Controller
    Added views for both controllers. These views are just dummy views at the moment.

---
+++ src/main/java/org/dnacronym/insertproduct/controllers/MenuController.java
@@ -0,0 +1,19 @@
+package org.dnacronym.insertproduct.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class MenuController implements Initializable {
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    private void fileAction(ActionEvent event) {
+
+    }
+}
---
+++ src/main/java/org/dnacronym/insertproduct/runnable/DNAApplication.java
@@ -0,0 +1,16 @@
+package org.dnacronym.insertproduct.runnable;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class DNAApplication extends Application {
+
+    private static final String TITLE = "DNA";
+
+    private static final String APPLICATION_VIEW = "/view/main_view.fxml";
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
