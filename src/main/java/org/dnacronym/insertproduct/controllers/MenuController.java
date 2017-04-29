4317868eae160c6af4c0307895f19a4c34a612b7
Joel Abrahams <abrahamsjo@gmail.com>
2017-04-29 15:34:14 +0200

    Make openAction a protected method
    This greatly increases testability.

--- src/main/java/org/dnacronym/insertproduct/controllers/MenuController.java
+++ src/main/java/org/dnacronym/insertproduct/controllers/MenuController.java
@@ -17,6 +17,6 @@ public class MenuController implements Initializable {
     }
 
     @FXML
-    private void openAction(ActionEvent event) {
+    protected void openAction(ActionEvent event) {
     }
 }
