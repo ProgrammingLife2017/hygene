7852d612292430d64acbcbbf03a6cbb87d062a55
Felix Dekker <felix@fwdekker.com>
2017-04-29 16:14:09 +0200

    Add unit tests for Link and Segment

---
+++ src/test/java/org/dnacronym/insertproductname/parser/LinkTest.java
@@ -0,0 +1,86 @@
+package org.dnacronym.insertproductname.parser;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+/**
+ * Unit tests for {@code Link}s.
+ *
+ * @author Felix Dekker
+ */
+final class LinkTest {
+    private Segment from;
+    private Segment to;
+
+
+    @BeforeEach
+    void beforeEach() {
+        from = mock(Segment.class);
+        to = mock(Segment.class);
+
+        when(from.getLength()).thenReturn(1000);
+        when(to.getLength()).thenReturn(1000);
+    }
+
+
+    @Test
+    void testNullFrom() {
+        final Throwable e = catchThrowable(() -> new Link(null, true, to, true, 341));
+        assertThat(e).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void testNullTo() {
+        final Throwable e = catchThrowable(() -> new Link(from, false, null, true, 884));
+        assertThat(e).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void testNegativeOverlap() {
+        final Throwable e = catchThrowable(() -> new Link(from, true, to, false, -426));
+        assertThat(e).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void testExcessiveOverlap() {
+        final Throwable e = catchThrowable(() -> new Link(from, false, to, true, 1001));
+        assertThat(e).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void testGetFrom() {
+        final Link link = new Link(from, true, to, false, 973);
+        assertThat(link.getFrom()).isEqualTo(from);
+    }
+
+    @Test
+    void testGetFromOrient() {
+        final Link link = new Link(from, false, to, false, 790);
+        assertThat(link.getFromOrient()).isEqualTo(false);
+    }
+
+    @Test
+    void testGetTo() {
+        final Link link = new Link(from, true, to, true, 5);
+        assertThat(link.getTo()).isEqualTo(to);
+    }
+
+    @Test
+    void testGetToOrient() {
+        final Link link = new Link(from, false, to, false, 707);
+        assertThat(link.getToOrient()).isEqualTo(false);
+    }
+
+    @Test
+    void testGetOverlap() {
+        final Link link = new Link(from, true, to, true, 649);
+        assertThat(link.getOverlap()).isEqualTo(649);
+    }
+}
---
+++ src/test/java/org/dnacronym/insertproductname/parser/SegmentTest.java
@@ -0,0 +1,56 @@
+package org.dnacronym.insertproductname.parser;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+
+/**
+ * Unit tests for {@code Segment}s.
+ *
+ * @author Felix Dekker
+ */
+class SegmentTest {
+    @Test
+    void testNullName() {
+        final Throwable e = catchThrowable(() -> new Segment(null, "KQpD5PbkOovX21f1losl"));
+        assertThat(e).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void testNullSequence() {
+        final Throwable e = catchThrowable(() -> new Segment("xpynu2oSI4xEMGya0aO7", null));
+        assertThat(e).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void testGetName() {
+        final Segment segment = new Segment("94ShLJ3ytpoXBiQxaxJ1", "aTeYVYR38z6AWbV2OgGh");
+        assertThat(segment.getName()).isEqualTo("94ShLJ3ytpoXBiQxaxJ1");
+    }
+
+    @Test
+    void testGetSequence() {
+        final Segment segment = new Segment("VWTkg5SAIpPwMNH5e3zQ", "dLiPH1qrIMISxxFqfbaa");
+        assertThat(segment.getSequence()).isEqualTo("dLiPH1qrIMISxxFqfbaa");
+    }
+
+    @Test
+    void testGetLength() {
+        final Segment segment = new Segment("gqkrQuzvtvjZLdOa8hDV", "vaKlWKSE0TAcFFjqfp4N");
+        assertThat(segment.getLength()).isEqualTo(20);
+    }
+
+    @Test
+    void testGetReversedSequence() {
+        final Segment segment = new Segment("xN3zLSN73ulhUMZ0zPac", "QXr99QbUGX521Gfbbh4B");
+        assertThat(segment.getReversedSequence()).isEqualTo("B4hbbfG125XGUbQ99rXQ");
+    }
+
+    @Test
+    void testGetReversedSequenceEmpty() {
+        final Segment segment = new Segment("FKjEclNC4G25BfsepUQS", "");
+        assertThat(segment.getReversedSequence()).isEqualTo("");
+    }
+}
