83b197d00e19e6e34acaf87635bfe2138a51593b
Joel Abrahams <abrahamsjo@gmail.com>
2017-04-29 13:56:00 +0200

    Add progress bar to splash screen

--- src/main/java/org/dnacronym/insertproduct/runnable/DNAApplication.java
+++ src/main/java/org/dnacronym/insertproduct/runnable/DNAApplication.java
@@ -2,18 +2,27 @@ package org.dnacronym.insertproduct.runnable;
 
 import com.sun.javafx.application.LauncherImpl;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 import org.dnacronym.insertproduct.preloader.DNAPreloader;
 
 public class DNAApplication extends Application {
 
-    private static final String TITLE = "DNA";
+    public static final String TITLE = "DNA";
 
     private static final String APPLICATION_VIEW = "/view/main_view.fxml";
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource(APPLICATION_VIEW));
 
+        primaryStage.setTitle(TITLE);
+        primaryStage.setMaximized(true);
+
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
