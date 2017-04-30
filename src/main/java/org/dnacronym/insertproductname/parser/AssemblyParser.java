21e4485eabfff2cb6accf3c4d96deac4e522796f
Felix Dekker <felix@fwdekker.com>
2017-04-30 17:44:20 +0200

    Extract code to methods and add docs

--- src/main/java/org/dnacronym/insertproductname/parser/AssemblyParser.java
+++ src/main/java/org/dnacronym/insertproductname/parser/AssemblyParser.java
@@ -11,6 +11,12 @@ import java.util.Map;
  * .
  */
 public final class AssemblyParser {
+    /**
+     * Translates a {@code Assembly} into a {@code GraphSequence}.
+     *
+     * @param assembly an {@code Assembly}
+     * @return a {@code GraphSequence}
+     */
     public SequenceGraph parse(final Assembly assembly) {
         final Map<String, SequenceNode> nodes = new HashMap<>();
 
@@ -22,15 +28,18 @@ public final class AssemblyParser {
             toNode.addLeftNeighbour(fromNode);
         }
 
-        SequenceNode first = getNode(nodes, assembly.getLinks().get(0).getFrom());
-        while (first.getLeftNeighbours().size() != 0) {
-            first = first.getLeftNeighbours().get(0);
-        }
-
-        return new SequenceGraph(first);
+        return new SequenceGraph(getFirstNode(nodes));
     }
 
 
+    /**
+     * Returns the {@code SequenceNode} corresponding to the given {@code Segment} from the {@code Map}, or adds it to
+     * the {@code Map} if it does not exist.
+     *
+     * @param nodes   a {@code Map} of {@code SequenceNode}s
+     * @param segment the {@code Segment} to look for
+     * @return {@code SequenceNode} corresponding to the given {@code Segment}
+     */
     private SequenceNode getNode(final Map<String, SequenceNode> nodes, final Segment segment) {
         final SequenceNode node = nodes.get(segment.getName());
 
@@ -41,5 +50,35 @@ public final class AssemblyParser {
         final SequenceNode node2 = new SequenceNode(segment.getSequence());
         nodes.put(segment.getName(), node2);
         return node2;
+    }
+
+    /**
+     * Calls {@code getFirstNode} on some element in the given {@code Map}.
+     *
+     * @param nodes a {@code Map} of {@code SequenceNode}s
+     * @return the indirect left neighbour of a {@code SequenceNode} that does not have a left neighbour
+     */
+    private SequenceNode getFirstNode(final Map<String, SequenceNode> nodes) {
+        return nodes.values().stream()
+                .findFirst()
+                .map(this::getFirstNode)
+                .orElse(null);
+    }
+
+    /**
+     * Takes a {@code SequenceNode}'s left neighbour and takes that neighbour's left neighbour etc. until it finds a
+     * {@code SequenceNode} without a left neighbour, and returns this {@code SequenceNode}.
+     *
+     * @param node a {@code Sequence Node}
+     * @return the indirect left neighbour of a {@code SequenceNode} that does not have a left neighbour
+     */
+    private SequenceNode getFirstNode(final SequenceNode node) {
+        SequenceNode first = node;
+
+        while (first.getLeftNeighbours().size() != 0) {
+            first = first.getLeftNeighbours().get(0);
+        }
+
+        return first;
     }
 }
