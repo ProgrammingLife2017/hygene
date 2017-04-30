6d9a5175ad63797c3b46bd70d04736b0c4118f6c
Georgios Andreadis <g.andreadis@student.tudelft.nl>
2017-04-30 09:33:10 +0200

    Add the SequenceGraph datastructure

---
+++ src/main/java/org/dnacronym/insertproductname/models/SequenceGraph.java
@@ -0,0 +1,29 @@
+package org.dnacronym.insertproductname.models;
+
+
+/**
+ * A {@code SequenceGraph} contains {@code SequenceNode}s.
+ */
+public final class SequenceGraph {
+    private final SequenceNode startNode;
+
+
+    /**
+     * Constructs a new {@code SequenceGraph} with the given starting node.
+     *
+     * @param startNode the first link of the segment chain
+     */
+    public SequenceGraph(final SequenceNode startNode) {
+        this.startNode = startNode;
+    }
+
+
+    /**
+     * Returns the start node.
+     *
+     * @return the start node.
+     */
+    public final SequenceNode getStartNode() {
+        return startNode;
+    }
+}
