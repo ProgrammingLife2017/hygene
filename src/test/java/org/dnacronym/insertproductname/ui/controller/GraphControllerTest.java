9ca9f84c5a9b53aff9eb4eecf1a5d8631e63212d
Joel Abrahams <abrahamsjo@gmail.com>
2017-05-01 22:11:01 +0200

    Add TestFX to project and dummy tests
    TestFX can be found here: https://github.com/TestFX/TestFX

---
+++ src/test/java/org/dnacronym/insertproductname/ui/controller/GraphControllerTest.java
@@ -0,0 +1,16 @@
+package org.dnacronym.insertproductname.ui.controller;
+
+import javafx.stage.Stage;
+import org.testfx.framework.junit.ApplicationTest;
+
+
+/**
+ * Unit tests for {@link GraphController}s.
+ */
+public class GraphControllerTest extends ApplicationTest {
+
+    @Override
+    public void start(final Stage stage) throws Exception {
+        // TODO write start method
+    }
+}
---
+++ src/test/java/org/dnacronym/insertproductname/ui/controller/MenuControllerTest.java
@@ -0,0 +1,16 @@
+package org.dnacronym.insertproductname.ui.controller;
+
+import javafx.stage.Stage;
+import org.testfx.framework.junit.ApplicationTest;
+
+
+/**
+ * Unit tests for {@link MenuController}s.
+ */
+public class MenuControllerTest extends ApplicationTest {
+
+    @Override
+    public void start(final Stage stage) throws Exception {
+        // TODO write start method
+    }
+}
