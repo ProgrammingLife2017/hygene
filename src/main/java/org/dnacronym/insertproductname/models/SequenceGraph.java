83f89465cfe3421569f0e33990161999f85aecfc
Georgios Andreadis <g.andreadis@student.tudelft.nl>
2017-05-01 19:01:48 +0200

    Add a map of nodes to 'SequenceGraph'

--- src/main/java/org/dnacronym/insertproductname/models/SequenceGraph.java
+++ src/main/java/org/dnacronym/insertproductname/models/SequenceGraph.java
@@ -1,10 +1,14 @@
 package org.dnacronym.insertproductname.models;
 
+import java.util.HashMap;
+import java.util.Map;
+
 
 /**
  * A {@code SequenceGraph} contains {@code SequenceNode}s.
  */
 public final class SequenceGraph {
+    private final Map<String, SequenceNode> nodes;
     private final SequenceNode startNode;
     private final SequenceNode endNode;
 
@@ -16,6 +20,7 @@ public final class SequenceGraph {
      * @param endNode the last link of the segment chain
      */
     public SequenceGraph(final SequenceNode startNode, final SequenceNode endNode) {
+        this.nodes = new HashMap<>();
         this.startNode = startNode;
         this.endNode = endNode;
     }
@@ -37,5 +42,26 @@ public final class SequenceGraph {
      */
     public SequenceNode getEndNode() {
         return endNode;
+    }
+
+    /**
+     * Returns the nodes.
+     * <p>
+     * At a later stage (after an initial prototype of the parser has been built), this datastructure will be better
+     * encapsulated by this class, and this accessor removed.
+     *
+     * @return the nodes.
+     */
+    public final Map<String, SequenceNode> getNodes() {
+        return nodes;
+    }
+
+    /**
+     * Returns the size of the graph, measured in terms of the number of nodes.
+     *
+     * @return the number of nodes in the graph.
+     */
+    public final int size() {
+        return nodes.size();
     }
 }
