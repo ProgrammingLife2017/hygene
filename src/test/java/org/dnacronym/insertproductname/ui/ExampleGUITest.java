a30fdd6e901d56c873d072c18a9783a11a1d5015
Joel Abrahams <abrahamsjo@gmail.com>
2017-05-03 23:25:39 +0200

    Add example GUI Test

---
+++ src/test/java/org/dnacronym/insertproductname/ui/ExampleGUITest.java
@@ -0,0 +1,44 @@
+package org.dnacronym.insertproductname.ui;
+
+import javafx.application.Platform;
+import org.dnacronym.insertproductname.ui.runnable.DNAApplication;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.testfx.api.FxRobot;
+import org.testfx.api.FxToolkit;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class ExampleGUITest extends FxRobot {
+
+    @BeforeAll
+    static void beforeAll() throws TimeoutException {
+        FxToolkit.registerPrimaryStage();
+        FxToolkit.showStage();
+    }
+
+    @BeforeEach
+    final void beforeEach() throws IOException, TimeoutException {
+        FxToolkit.setupApplication(DNAApplication.class);
+    }
+
+    @AfterEach
+    final void afterEach() throws TimeoutException {
+        FxToolkit.hideStage();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        Platform.exit();
+    }
+
+
+    @Test
+    public final void simpleFxRobotSample() {
+        moveTo("#fileMenu");
+    }
+}
