a17223286a5444b03e25dcf63105c3e61e722308
Joel Abrahams <abrahamsjo@gmail.com>
2017-05-05 13:08:07 +0200

    Remove redundant newlines in ExampleGUITest

--- src/test/java/org/dnacronym/insertproductname/ui/ExampleGUITest.java
+++ src/test/java/org/dnacronym/insertproductname/ui/ExampleGUITest.java
@@ -14,8 +14,6 @@ import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
 public class ExampleGUITest extends FxRobot {
-
-
     @BeforeAll
     static void beforeAll() throws TimeoutException {
         FxToolkit.registerPrimaryStage();
