86d9dd8267a40761b4ac034d9976ba9f758e02ef
Joel Abrahams <abrahamsjo@gmail.com>
2017-05-05 18:32:34 +0200

    Remove Swing Pane in GraphController

--- src/main/java/org/dnacronym/insertproductname/ui/controller/GraphController.java
+++ src/main/java/org/dnacronym/insertproductname/ui/controller/GraphController.java
@@ -10,10 +10,8 @@ import org.dnacronym.insertproductname.models.SequenceGraph;
 import org.dnacronym.insertproductname.ui.runnable.DNAApplication;
 import org.dnacronym.insertproductname.ui.store.GraphStore;
 import org.dnacronym.insertproductname.ui.visualizer.GraphStreamVisualiser;
-import org.graphstream.ui.view.View;
-import org.graphstream.ui.view.Viewer;
 
-import javax.swing.JComponent;
+import javax.swing.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -26,8 +24,6 @@ public final class GraphController implements Initializable {
 
     private @MonotonicNonNull GraphStore graphStore;
 
-    private @MonotonicNonNull SwingNode swingNode;
-
     @FXML
     private @MonotonicNonNull Pane graphPane;
 
@@ -36,12 +32,9 @@ public final class GraphController implements Initializable {
     public void initialize(final URL location, final ResourceBundle resources) {
         setGraphStore(DNAApplication.getGraphStore());
 
-        swingNode = new SwingNode();
         visualiser = new GraphStreamVisualiser(true, true);
 
         if (graphPane != null && graphStore != null) {
-            graphPane.getChildren().add(swingNode);
-
             graphStore.getSequenceGraphProperty().addListener((observable, oldGraph, newGraph) -> {
                 if (newGraph != null) {
                     updateGraphSwingNode(newGraph);
@@ -66,22 +59,13 @@ public final class GraphController implements Initializable {
      * @param sequenceGraph new {@link SequenceGraph} to display.
      */
     protected void updateGraphSwingNode(@NonNull final SequenceGraph sequenceGraph) {
-        if (graphPane == null || visualiser == null || swingNode == null) {
+        if (graphPane == null || visualiser == null) {
             return;
         }
 
-        visualiser.populateGraph(sequenceGraph);
-
-        final Viewer viewer = new Viewer(visualiser.getGraph(), Viewer.ThreadingModel.GRAPH_IN_ANOTHER_THREAD);
-        final View view = viewer.addDefaultView(false);
-
-        viewer.getDefaultView().setSize(
-                (int) Math.round(graphPane.getWidth()),
-                (int) Math.round(graphPane.getHeight()));
-
-        viewer.enableAutoLayout();
-        viewer.setCloseFramePolicy(Viewer.CloseFramePolicy.EXIT);
-
-        swingNode.setContent((JComponent) view);
+        SwingUtilities.invokeLater(() -> {
+            visualiser.populateGraph(sequenceGraph);
+            visualiser.getGraph().display();
+        });
     }
 }
