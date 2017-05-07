3b9e9fce929b7b83bb08133f02300deb4a9ddab4
Felix Dekker <felix@fwdekker.com>
2017-05-07 13:13:43 +0200

    Unremove GUI tests

---
+++ src/test/java/org/dnacronym/hygene/ui/ExampleGUITest.java
@@ -0,0 +1,42 @@
+package org.dnacronym.hygene.ui;
+
+import org.dnacronym.hygene.ui.runnable.DNAApplication;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.testfx.api.FxRobot;
+import org.testfx.api.FxToolkit;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class ExampleGUITest extends FxRobot {
+    @BeforeAll
+    static void beforeAll() throws TimeoutException {
+        FxToolkit.registerPrimaryStage();
+        FxToolkit.showStage();
+    }
+
+    @BeforeEach
+    final void beforeEach() throws IOException, TimeoutException {
+        FxToolkit.setupApplication(DNAApplication.class);
+    }
+
+    @AfterEach
+    final void afterEach() throws TimeoutException {
+        FxToolkit.hideStage();
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.exit(0);
+    }
+
+
+    @Test
+    public final void simpleFxRobotSample() {
+        moveTo("#fileMenu");
+    }
+}
