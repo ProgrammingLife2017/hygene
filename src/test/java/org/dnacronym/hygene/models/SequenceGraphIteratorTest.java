fe6860e23f63cb1a84158bfe4571e99464f0c1d1
Felix Dekker <felix@fwdekker.com>
2017-05-08 14:38:06 +0200

    Add new iterator tests

--- src/test/java/org/dnacronym/hygene/models/SequenceGraphIteratorTest.java
+++ src/test/java/org/dnacronym/hygene/models/SequenceGraphIteratorTest.java
@@ -5,14 +5,34 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
 
 /**
  * Unit tests for the iterators in {@code SequenceGraph}s.
  */
 class SequenceGraphIteratorTest {
+    private static final Consumer<SequenceNode> DUPLICATE_MODIFIER = node -> node.addReadIdentifier("");
+    private static final Function<SequenceNode, Boolean> DUPLICATE_DETECTOR =
+            node -> !node.getReadIdentifiers().isEmpty();
+
+
+    @Test
+    void testIteratorEmptyException() {
+        final SequenceGraph graph = new SequenceGraph(new ArrayList<>());
+
+        final Throwable e = catchThrowable(() -> graph.iterator().next());
+        assertThat(e).isInstanceOf(NoSuchElementException.class);
+    }
+
+
     @Test
     void testIteratorEmpty() {
         final SequenceGraph graph = new SequenceGraph(new ArrayList<>());
@@ -79,6 +99,25 @@ class SequenceGraphIteratorTest {
 
 
     @Test
+    void testIteratorDuplicateDiamond() {
+        final SequenceNode nodeA = new SequenceNode("1", "1");
+        final SequenceNode nodeB = new SequenceNode("2", "2");
+        final SequenceNode nodeC = new SequenceNode("3", "3");
+        final SequenceNode nodeD = new SequenceNode("3", "3");
+        nodeA.linkToRightNeighbour(nodeB);
+        nodeA.linkToRightNeighbour(nodeC);
+        nodeB.linkToRightNeighbour(nodeD);
+        nodeC.linkToRightNeighbour(nodeD);
+
+        final SequenceGraph graph = new SequenceGraph(Arrays.asList(nodeA, nodeB, nodeC, nodeD));
+        final List<SequenceNode> nodes = iterateModifyCollect(graph.iterator(DUPLICATE_DETECTOR), DUPLICATE_MODIFIER);
+
+        assertThat(nodes).hasSize(4);
+        assertThat(nodes).containsExactly(nodeA, nodeB, nodeC, nodeD);
+    }
+
+
+    @Test
     void testReverseIteratorEmpty() {
         final SequenceGraph graph = new SequenceGraph(new ArrayList<>());
         assertThat(graph.reverseIterator()).hasSize(0);
@@ -140,5 +179,61 @@ class SequenceGraphIteratorTest {
 
         assertThat(graph.reverseIterator()).hasSize(5);
         assertThat(graph.reverseIterator()).containsExactly(nodeD, nodeB, nodeC, nodeA, nodeA);
+    }
+
+
+    @Test
+    void testReverseIteratorDuplicateParallelElements() {
+        final SequenceNode nodeA = new SequenceNode("1", "1");
+        final SequenceNode nodeB = new SequenceNode("2", "2");
+        final SequenceNode nodeC = new SequenceNode("3", "3");
+        final SequenceNode nodeD = new SequenceNode("4", "4");
+        nodeA.linkToRightNeighbour(nodeB);
+        nodeA.linkToRightNeighbour(nodeC);
+        nodeA.linkToRightNeighbour(nodeD);
+
+        final SequenceGraph graph = new SequenceGraph(Arrays.asList(nodeA, nodeB, nodeC, nodeD));
+        final List<SequenceNode> nodes = iterateModifyCollect(graph.reverseIterator(DUPLICATE_DETECTOR),
+                DUPLICATE_MODIFIER);
+
+        assertThat(nodes).hasSize(4);
+        assertThat(nodes).containsExactly(nodeB, nodeC, nodeD, nodeA);
+    }
+
+    @Test
+    void testReverseIteratorDuplicateDiamond() {
+        final SequenceNode nodeA = new SequenceNode("1", "1");
+        final SequenceNode nodeB = new SequenceNode("2", "2");
+        final SequenceNode nodeC = new SequenceNode("3", "3");
+        final SequenceNode nodeD = new SequenceNode("3", "3");
+        nodeA.linkToRightNeighbour(nodeB);
+        nodeA.linkToRightNeighbour(nodeC);
+        nodeB.linkToRightNeighbour(nodeD);
+        nodeC.linkToRightNeighbour(nodeD);
+
+        final SequenceGraph graph = new SequenceGraph(Arrays.asList(nodeA, nodeB, nodeC, nodeD));
+        final List<SequenceNode> nodes = iterateModifyCollect(graph.reverseIterator(DUPLICATE_DETECTOR),
+                DUPLICATE_MODIFIER);
+
+        assertThat(nodes).hasSize(4);
+        assertThat(nodes).containsExactly(nodeD, nodeB, nodeC, nodeA);
+    }
+
+
+    /**
+     * Applies the {@code modifier} to each element it iterates over during iteration, and collects the results.
+     *
+     * @param iterator an {@code Iterator} over {@code SequenceNode}s
+     * @param modifier a {@code Consumer} of {@code SequenceNode}s
+     * @return the {@code SequenceNode}s returned by the {@code Iterator}
+     */
+    private List<SequenceNode> iterateModifyCollect(final Iterator<SequenceNode> iterator,
+                                                    final Consumer<SequenceNode> modifier) {
+        final List<SequenceNode> nodes = new ArrayList<>();
+        iterator.forEachRemaining(node -> {
+            nodes.add(node);
+            modifier.accept(node);
+        });
+        return nodes;
     }
 }
