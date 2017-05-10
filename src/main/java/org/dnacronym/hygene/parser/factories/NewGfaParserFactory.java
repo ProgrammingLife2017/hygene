fc1a52a8233109d456190d32ec23e5785f82874a
Casper Boone <mail@casperboone.nl>
2017-05-10 23:22:22 +0200

    Add new GfaFile class

---
+++ src/main/java/org/dnacronym/hygene/parser/factories/NewGfaParserFactory.java
@@ -0,0 +1,42 @@
+package org.dnacronym.hygene.parser.factories;
+
+import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
+import org.dnacronym.hygene.parser.NewGfaParser;
+
+
+/**
+ * Constructs {@code GfaParser} objects.
+ */
+public final class NewGfaParserFactory {
+    private static @MonotonicNonNull NewGfaParser instance;
+
+
+    /**
+     * Makes class non instantiable.
+     */
+    private NewGfaParserFactory() {
+    }
+
+
+    /**
+     * Sets a custom instance of {@code GfaParser}.
+     *
+     * @param gfaParser custom instance of {@code GfaParser}
+     */
+    public static void setInstance(final NewGfaParser gfaParser) {
+        instance = gfaParser;
+    }
+
+    /**
+     * Returns a new instance of {@code GfaParser} if no preferred instance is set.
+     *
+     * @return an instance of {@code GfaParser}
+     */
+    public static NewGfaParser getInstance() {
+        if (instance == null) {
+            return new NewGfaParser();
+        }
+
+        return instance;
+    }
+}
---
+++ src/test/java/org/dnacronym/hygene/parser/NewGfaFileTest.java
@@ -0,0 +1,87 @@
+package org.dnacronym.hygene.parser;
+
+import org.dnacronym.hygene.models.EdgeMetadata;
+import org.dnacronym.hygene.models.NodeMetadata;
+import org.dnacronym.hygene.parser.factories.MetadataParserFactory;
+import org.dnacronym.hygene.parser.factories.NewGfaParserFactory;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.catchThrowable;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+
+class NewGfaFileTest {
+    private final static String SIMPLE_GFA_CONTENTS = String.format("H\tVN:Z:1.0%n"
+            + "S\t11\tACCTT%n"
+            + "S\t12\tTCAAGG%n"
+            + "L\t11\t+\t12\t-\t4M%n");
+
+    @Test
+    void testGfaFileObjectCanBeConstructed() {
+        final NewGfaFile gfaFile = new NewGfaFile("name_of_the_file.gfa");
+
+        assertThat(gfaFile.getFileName()).isEqualTo("name_of_the_file.gfa");
+    }
+
+    @Test
+    void testCannotReadNonExistingFile() {
+        final Throwable e = catchThrowable(() -> new NewGfaFile("random-file-name").parse());
+
+        assertThat(e).isInstanceOf(ParseException.class);
+    }
+
+    @Test
+    void testGetGraphWithoutParsing() {
+        final Throwable e = catchThrowable(() -> new NewGfaFile("random-file-name").getGraph());
+
+        assertThat(e).isInstanceOf(ParseException.class);
+    }
+
+    @Test
+    void testReadFile() throws ParseException {
+        NewGfaFile gfaFile = new NewGfaFile("src/test/resources/gfa/simple.gfa");
+
+        assertThat(gfaFile.readFile()).isEqualTo(SIMPLE_GFA_CONTENTS);
+    }
+
+    @Test
+    void testParseFile() throws IOException, ParseException {
+        NewGfaParser gfaParser = spy(NewGfaParser.class);
+        NewGfaParserFactory.setInstance(gfaParser);
+
+        NewGfaFile gfaFile = new NewGfaFile("src/test/resources/gfa/simple.gfa");
+        gfaFile.parse();
+
+        verify(gfaParser).parse(any(NewGfaFile.class));
+        assertThat(gfaFile.getGraph()).isNotNull();
+    }
+
+    @Test
+    void testParseNodeMetaData() throws IOException, ParseException {
+        MetadataParser metadataParser = spy(MetadataParser.class);
+        MetadataParserFactory.setInstance(metadataParser);
+
+        NewGfaFile gfaFile = new NewGfaFile("src/test/resources/gfa/simple.gfa");
+        NodeMetadata nodeMetadata = gfaFile.parseNodeMetadata(2);
+
+        verify(metadataParser).parseNodeMetadata(SIMPLE_GFA_CONTENTS, 2);
+        assertThat(nodeMetadata.getSequence()).isEqualTo("ACCTT");
+    }
+
+    @Test
+    void testParseEdgeMetadata() throws IOException, ParseException {
+        MetadataParser metadataParser = spy(MetadataParser.class);
+        MetadataParserFactory.setInstance(metadataParser);
+
+        NewGfaFile gfaFile = new NewGfaFile("src/test/resources/gfa/simple.gfa");
+        EdgeMetadata edgeMetadata = gfaFile.parseEdgeMetadata(4);
+
+        verify(metadataParser).parseEdgeMetadata(SIMPLE_GFA_CONTENTS, 4);
+        assertThat(edgeMetadata.getToOrient()).isEqualTo("-");
+    }
+}
