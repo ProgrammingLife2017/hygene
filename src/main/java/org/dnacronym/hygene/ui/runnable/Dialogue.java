6a6b310293b33a34cee2d347c6bb34941cfa1e89
Joel Abrahams <abrahamsjo@gmail.com>
2017-05-10 21:15:32 +0200

    Move enum to bottom to Dialogue

--- src/main/java/org/dnacronym/hygene/ui/runnable/Dialogue.java
+++ src/main/java/org/dnacronym/hygene/ui/runnable/Dialogue.java
@@ -5,39 +5,11 @@ import javafx.scene.control.Alert;
 
 
 /**
- * A dialogue is displayed onscreen and prevents any further interaction with the application until it is closed.
+ * A dialogue is displayed on-screen and prevents any further interaction with the application until it is closed.
  * <p>
  * A dialogue can either be of dialogueType {@link DialogueType#ERROR} or {@link DialogueType#WARNING}.
  */
 public class Dialogue {
-    /**
-     * Denotes the dialogueType of dialogue.
-     */
-    public enum DialogueType {
-        /**
-         * An error is more severe, and should be used to relay to the user that something went
-         * wrong with the application.
-         */
-        ERROR("Error", Alert.AlertType.ERROR),
-        /**
-         * A warning is less severe, and should be used to relay to the user that they did
-         * something they shouldn't do.
-         */
-        WARNING("Warning", Alert.AlertType.WARNING);
-
-        private final String title;
-        private final Alert.AlertType alertType;
-
-        /**
-         * @param title     name of the dialogue type.
-         * @param alertType JavaFX {@link javafx.scene.control.Alert.AlertType} of this dialogue type.
-         */
-        DialogueType(final String title, final Alert.AlertType alertType) {
-            this.title = title;
-            this.alertType = alertType;
-        }
-    }
-
     private final DialogueType dialogueType;
     private final Exception exception;
 
@@ -84,5 +56,33 @@ public class Dialogue {
         alert.setHeaderText(headerText);
 
         alert.showAndWait();
+    }
+
+    /**
+     * Denotes the dialogueType of dialogue.
+     */
+    public enum DialogueType {
+        /**
+         * An error is more severe, and should be used to relay to the user that something went
+         * wrong with the application.
+         */
+        ERROR("Error", Alert.AlertType.ERROR),
+        /**
+         * A warning is less severe, and should be used to relay to the user that they did
+         * something they shouldn't do.
+         */
+        WARNING("Warning", Alert.AlertType.WARNING);
+
+        private final String title;
+        private final Alert.AlertType alertType;
+
+        /**
+         * @param title     name of the dialogue type.
+         * @param alertType JavaFX {@link javafx.scene.control.Alert.AlertType} of this dialogue type.
+         */
+        DialogueType(final String title, final Alert.AlertType alertType) {
+            this.title = title;
+            this.alertType = alertType;
+        }
     }
 }
