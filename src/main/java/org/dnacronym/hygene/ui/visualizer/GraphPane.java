4c0a5524c1b5e51ee8ccb39d7a5684c27bc09b5c
Joel Abrahams <abrahamsjo@gmail.com>
2017-05-10 10:07:15 +0200

    Add recursive drawing methods

--- src/main/java/org/dnacronym/hygene/ui/visualizer/GraphPane.java
+++ src/main/java/org/dnacronym/hygene/ui/visualizer/GraphPane.java
@@ -1,29 +1,34 @@
 package org.dnacronym.hygene.ui.visualizer;
 
+import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleObjectProperty;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
-import org.checkerframework.checker.nullness.qual.NonNull;
 import org.dnacronym.hygene.models.SequenceGraph;
 import org.dnacronym.hygene.models.SequenceNode;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 
 /**
  * A simple canvas that allows drawing of primitive shapes.
+ * <p>
  * When passing a {@link SequenceGraph}, it will draw it using JavaFX primitives.
  *
  * @see Canvas
  * @see GraphicsContext
  */
 public class GraphPane extends Pane {
+    private static final double DEFAULT_NODE_HEIGHT = 100;
+
     private final Canvas canvas;
     private final GraphicsContext graphicsContext;
 
     private final ObjectProperty<Color> edgeColorProperty;
+    private final DoubleProperty nodeHeightProperty;
+
 
     /**
      * Create a new {@link GraphPane} instance.
@@ -39,39 +44,77 @@ public class GraphPane extends Pane {
         canvas.heightProperty().bind(this.heightProperty());
 
         edgeColorProperty = new SimpleObjectProperty<>(Color.BLACK);
+        nodeHeightProperty = new SimpleDoubleProperty(DEFAULT_NODE_HEIGHT);
 
         this.getChildren().add(canvas);
     }
 
 
     /**
-     * Draw line onscreen.
+     * Draw line on the {@link Canvas}.
      *
      * @param startX x position of the start of the line.
      * @param startY y position of the start of the line.
      * @param endX   x position of the end of the line.
      * @param endY   y position of the end of the line.
-     * @param color  color of the line.
      */
-    private void drawLine(final double startX, final double startY,
-                          final double endX, final double endY, final Color color) {
-        graphicsContext.setFill(color);
+    private void drawEdge(final double startX, final double startY,
+                          final double endX, final double endY) {
         graphicsContext.strokeLine(startX, startY, endX, endY);
     }
 
     /**
-     * Draw a rectangle onscreen. The width and height should be given.
+     * draws all onscreen edges between the current {@link SequenceNode} and it's right neighbours.
+     * <p>
+     * Afterwards, calls itself on each of the right neighbours of the current node.
      *
-     * @param startX x position of the leftmost position of the rectangle.
-     * @param startY y position of the bottom of the rectangle.
+     * @param sequenceNode the node who's edges should be drawn on the {@link Canvas}.
+     * @see SequenceNode#getRightNeighbours()
+     */
+    private void drawEdges(final SequenceNode sequenceNode) {
+        // TODO iterate over neighbours, draw edges and call method on those neighbours
+    }
+
+    /**
+     * Sets the fill of the {@link GraphicsContext} before proceeding to draw all onscreen edges.
+     *
+     * @param sequenceNode the node representing the source of the graph.
+     * @param color        the color with which all edges should be drawn.
+     * @see #drawEdges(SequenceNode)
+     */
+    private void drawEdges(final SequenceNode sequenceNode, final Color color) {
+        graphicsContext.setFill(color);
+        drawEdges(sequenceNode);
+    }
+
+    /**
+     * Draw a rectangle on the {@link Canvas}.
+     *
+     * @param startX x position of the upper left corner of rectangle.
+     * @param startY y position of the upper left corner of rectangle.
      * @param width  width of the rectangle.
      * @param height height of the rectangle.
      * @param color  color of the rectangle.
      */
-    private void drawRectangle(final double startX, final double startY,
-                               final double width, final double height, final Color color) {
+    private void drawNode(final double startX, final double startY,
+                          final double width, final double height, final Color color) {
         graphicsContext.setFill(color);
         graphicsContext.fillRect(startX, startY, startX + width, startY - height);
+    }
+
+    /**
+     * Draw the given {@link SequenceNode} onscreen.
+     * <p>
+     * Color depends on the set color of he {@link SequenceNode}.
+     * <p>
+     * Afterwards, proceeds to draw all right neighbours of the given {@link SequenceNode}.
+     *
+     * @param sequenceNode the node which should be drawn onscreen.
+     * @param laneHeight   the heigh of a step. A single step represents a band onscreen.
+     * @see SequenceNode#getRightNeighbours()
+     */
+    private void drawNodes(final SequenceNode sequenceNode, final double laneHeight) {
+        // TODO draw node based on x, lane, width and its color. Iterate over right neighbours and call method again.
     }
 
     /**
@@ -83,15 +126,15 @@ public class GraphPane extends Pane {
 
     /**
      * Visualise a {@link SequenceNode} in the {@link Canvas}.
-     *
+     * <p>
      * A {@link SequenceNode} is drawn in the center of each band.
      *
      * @param sequenceNode sequenceNode to visualise.
      * @param stepHeight   denotes the height of each of the onscreen bands in which nodes reside.
      */
     private void visualise(final SequenceNode sequenceNode, final double stepHeight) {
-        // TODO call drawRectangle and drawEdge using the x and position of the node and neighbour
-        throw new NotImplementedException();
+        drawNodes(sequenceNode, stepHeight);
+        drawEdges(sequenceNode, edgeColorProperty.get());
     }
 
     /**
@@ -109,14 +152,14 @@ public class GraphPane extends Pane {
      *
      * @param sequenceGraph {@link SequenceGraph} to populate canvas with.
      */
-    public final void visualise(final @NonNull SequenceGraph sequenceGraph) {
+    public final void visualise(final SequenceGraph sequenceGraph) {
         clear();
 
         // TODO retrieve bandcount from sequenceGraph
         final double bandCount = 1;
-        final double stepHeight = canvas.getHeight() / bandCount;
+        final double laneHeight = canvas.getHeight() / bandCount;
 
         final SequenceNode sink = sequenceGraph.getSinkNode();
-        visualise(sink, stepHeight);
+        visualise(sink, laneHeight);
     }
 }
