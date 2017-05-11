f72c54e30a8e901f2b26257a42706356c7fd998c
Joel Abrahams <abrahamsjo@gmail.com>
2017-05-11 10:54:42 +0200

    Define FXML elements in Controller

--- src/main/java/org/dnacronym/hygene/ui/controller/ConfigController.java
+++ src/main/java/org/dnacronym/hygene/ui/controller/ConfigController.java
@@ -1,14 +1,23 @@
 package org.dnacronym.hygene.ui.controller;
 
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Slider;
+import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ConfigController implements Initializable {
+    @FXML
+    private @MonotonicNonNull Slider nodeHeight, nodeWidth;
+
+    @FXML
+    private @MonotonicNonNull ColorPicker edgeColors;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-
     }
 }
