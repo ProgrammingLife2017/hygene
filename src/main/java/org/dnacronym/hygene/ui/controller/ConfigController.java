53489014a7b96abb67d4a2c437034c6b9d9e98c9
Joel Abrahams <abrahamsjo@gmail.com>
2017-05-11 10:16:19 +0200

    Create simple config view

---
+++ src/main/java/org/dnacronym/hygene/ui/controller/ConfigController.java
@@ -0,0 +1,14 @@
+package org.dnacronym.hygene.ui.controller;
+
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ConfigController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
