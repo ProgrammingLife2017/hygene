efc2b1ce9453047823f21a807de52d564f1a0439
Joel Abrahams <abrahamsjo@gmail.com>
2017-05-11 11:39:57 +0200

    Store GraphPane in ConfigController

--- src/main/java/org/dnacronym/hygene/ui/controller/ConfigController.java
+++ src/main/java/org/dnacronym/hygene/ui/controller/ConfigController.java
@@ -6,18 +6,32 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Slider;
 import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
+import org.dnacronym.hygene.ui.visualizer.GraphPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ConfigController implements Initializable {
+    private @MonotonicNonNull GraphPane graphPane;
+
     @FXML
     private @MonotonicNonNull Slider nodeHeight, nodeWidth;
 
     @FXML
     private @MonotonicNonNull ColorPicker edgeColors;
+    @FXML
+    private @MonotonicNonNull Slider edgeWidth;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+    }
+
+    /**
+     * Set the {@link GraphPane}. This allows the sliders to change the properties of the {@link GraphPane}.
+     *
+     * @param graphPane graph pane to set in the controller.
+     */
+    void setGraphPane(final GraphPane graphPane) {
+        this.graphPane = graphPane;
     }
 }
