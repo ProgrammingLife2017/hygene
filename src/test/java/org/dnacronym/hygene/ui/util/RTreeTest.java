85235dda7b8e1f1c6439d964cc7687dbff715340
Casper Boone <mail@casperboone.nl>
2017-05-15 19:14:40 +0200

    Add RTree implementation

---
+++ src/test/java/org/dnacronym/hygene/ui/util/RTreeTest.java
@@ -0,0 +1,66 @@
+package org.dnacronym.hygene.ui.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+/**
+ * Tests the RTree integration.
+ */
+final class RTreeTest {
+    @Test
+    void testFindNode() {
+        RTree tree = new RTree();
+        tree.addNode(45, 10, 10, 20, 20);
+
+        tree.find(15, 15,
+                nodeId -> assertThat(nodeId).isEqualTo(45),
+                edgeLineNumber -> fail("Found an edge, while expecting a node")
+        );
+    }
+
+    @Test
+    void testFindNearNode() {
+        RTree tree = new RTree();
+        tree.addNode(45, 10, 10, 20, 20);
+
+        tree.find(9, 9,
+                nodeId -> assertThat(nodeId).isEqualTo(45),
+                edgeLineNumber -> fail("Found an edge, while expecting a node")
+        );
+    }
+
+    @Test
+    void testFindEdge() {
+        RTree tree = new RTree();
+        tree.addEdge(30, 25, 25, 50, 50);
+
+        tree.find(40, 40,
+                nodeId -> fail("Found a node, while expecting an edge"),
+                edgeLineNumber -> assertThat(edgeLineNumber).isEqualTo(30)
+        );
+    }
+
+    @Test
+    void testFindNearEdge() {
+        RTree tree = new RTree();
+        tree.addEdge(30, 25, 25, 50, 50);
+
+        tree.find(41, 40,
+                nodeId -> fail("Found a node, while expecting an edge"),
+                edgeLineNumber -> assertThat(edgeLineNumber).isEqualTo(30)
+        );
+    }
+
+    @Test
+    void testOnlyFindWithinMaxRadius() {
+        RTree tree = new RTree();
+        tree.addNode(0, 0, 0, 5, 5);
+
+        tree.find(1000, 1000,
+                nodeId -> fail("Found a node, while expecting no results"),
+                edgeLineNumber -> fail("Found an edge, while expecting no results")
+        );
+    }
+}
