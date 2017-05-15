44fdab1454d15ab4c7ba07dde683a9a7a4c3cd12
Casper Boone <mail@casperboone.nl>
2017-05-15 17:21:38 +0200

    Remove unused import

--- src/test/java/org/dnacronym/hygene/parser/NewGfaParserTest.java
+++ src/test/java/org/dnacronym/hygene/parser/NewGfaParserTest.java
@@ -11,7 +11,6 @@ import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
