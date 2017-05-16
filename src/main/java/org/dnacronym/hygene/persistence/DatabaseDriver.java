d5e6afb7d352ab66e448379329b937cb2acde661
Georgios Andreadis <g.andreadis@student.tudelft.nl>
2017-05-16 20:02:26 +0200

    Add file contents hash function

--- src/main/java/org/dnacronym/hygene/persistence/DatabaseDriver.java
+++ src/main/java/org/dnacronym/hygene/persistence/DatabaseDriver.java
@@ -1,8 +1,11 @@
 package org.dnacronym.hygene.persistence;
 
 import javafx.util.Pair;
+import org.apache.commons.codec.digest.DigestUtils;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -17,6 +20,7 @@ import java.util.stream.Collectors;
 final class DatabaseDriver {
     private static final String DB_FILE_EXTENSION = ".db";
 
+    private final String fileName;
     private final Connection connection;
     private final List<DatabaseTable> tables;
 
@@ -25,14 +29,14 @@ final class DatabaseDriver {
      * Constructs a DatabaseDriver.
      *
      * @param fileName the file name of the corresponding GFA-file
-     * @param tables   the list of tables (assumed to have at least one element)
-     * @throws SQLException in the case of erroneous SQL behaviour
+     * @param tables   the list of tables (assumed to have at least one element)  @throws SQLException in the case of erroneous SQL behaviour
      */
     DatabaseDriver(final String fileName, List<DatabaseTable> tables) throws SQLException {
-        final boolean databaseAlreadyExisted = (new File(fileName + DB_FILE_EXTENSION)).exists();
-
-        connection = DriverManager.getConnection("jdbc:sqlite:" + fileName + DB_FILE_EXTENSION);
+        this.fileName = fileName;
         this.tables = tables;
+
+        final boolean databaseAlreadyExisted = (new File(fileName + DB_FILE_EXTENSION)).exists();
+        connection = DriverManager.getConnection("jdbc:sqlite:" + fileName + DB_FILE_EXTENSION);
 
         if (!databaseAlreadyExisted) {
             setupTables();
@@ -45,7 +49,7 @@ final class DatabaseDriver {
      *
      * @throws SQLException in the case of erroneous SQL behaviour
      */
-    void setupTables() throws SQLException {
+    private void setupTables() throws SQLException {
         final Statement statement = connection.createStatement();
 
         for (DatabaseTable table : tables) {
@@ -55,5 +59,19 @@ final class DatabaseDriver {
 
             statement.executeUpdate("CREATE TABLE IF NOT EXISTS " + table.getName() + "(" + columnList + ")");
         }
+    }
+
+    /**
+     * Computes the digest of the contents of the file this database belongs to.
+     *
+     * @return a digest, representing the contents of the file
+     * @throws IOException in the case of errors during IO operations
+     */
+    private String computeFileDigest() throws IOException {
+        final FileInputStream fileInputStream = new FileInputStream(new File(fileName));
+        final String digest = DigestUtils.sha512Hex(fileInputStream);
+        fileInputStream.close();
+
+        return digest;
     }
 }
