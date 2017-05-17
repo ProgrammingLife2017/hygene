57b7b1627a8ebe76facce8d9a400f56437929fe9
Joel Abrahams <abrahamsjo@gmail.com>
2017-05-17 15:46:29 +0200

    Add application logo

--- src/main/java/org/dnacronym/hygene/ui/runnable/Hygene.java
+++ src/main/java/org/dnacronym/hygene/ui/runnable/Hygene.java
@@ -5,6 +5,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -22,10 +23,13 @@ import java.net.URL;
  * @see LauncherImpl#launchApplication(Class, Class, String[])
  */
 public final class Hygene extends Application {
-    static final String TITLE = "Hygene";
-    private static final String APPLICATION_VIEW = "/ui/view/main_view.fxml";
     private static final Logger LOGGER = LogManager.getLogger(Hygene.class);
     private static @MonotonicNonNull Hygene hygene;
+
+    private static final String APPLICATION_VIEW = "/ui/view/main_view.fxml";
+    private static final String APPLICATION_ICON = "/ui/icons/hygene_logo_small.png";
+
+    static final String TITLE = "Hygene";
 
     private @MonotonicNonNull GraphStore graphStore;
     private @MonotonicNonNull GraphVisualizer graphVisualizer;
@@ -89,6 +93,10 @@ public final class Hygene extends Application {
 
         final Scene rootScene = new Scene(parent);
         primaryStage.setScene(rootScene);
+
+        final Image HygeneIcon = new Image(String.valueOf(Files.getInstance().getResourceUrl(APPLICATION_ICON)));
+        primaryStage.getIcons().add(HygeneIcon);
+
         primaryStage.show();
 
         LOGGER.info("Launching Hygene GUI");
