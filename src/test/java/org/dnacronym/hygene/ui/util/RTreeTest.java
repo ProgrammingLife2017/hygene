5787f01123da0de882ec8e3a0db838abc6180661
Casper Boone <mail@casperboone.nl>
2017-05-18 12:16:58 +0200

    Add new line in RTreeTest

--- src/test/java/org/dnacronym/hygene/ui/util/RTreeTest.java
+++ src/test/java/org/dnacronym/hygene/ui/util/RTreeTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
 
+
 /**
  * Tests the RTree integration.
  */
