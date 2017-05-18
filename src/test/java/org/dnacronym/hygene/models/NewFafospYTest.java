2f3b71c4fb100809a63fe52c9bc1001f1eb4398b
Felix Dekker <felix@fwdekker.com>
2017-05-18 14:15:05 +0200

    Move FAFOSP-Y to separate class

--- src/test/java/org/dnacronym/hygene/models/NewFafospYTest.java
+++ src/test/java/org/dnacronym/hygene/models/NewFafospYTest.java
@@ -33,9 +33,9 @@ class NewFafospYTest extends GraphBasedTest {
     void testSingleNode() {
         createGraph(1);
 
-        getGraph().fafospY();
+        getGraph().fafosp().vertical();
 
-        assertForEachNode(new int[] {1}, getUnscaledYPosition);
+        assertForEachNode(new int[]{1}, getUnscaledYPosition);
     }
 
     /**
@@ -46,11 +46,11 @@ class NewFafospYTest extends GraphBasedTest {
     @Test
     void testTwoSequential() {
         createGraph(2);
-        addEdges(new int[][] {{0, 1}});
+        addEdges(new int[][]{{0, 1}});
 
-        getGraph().fafospY();
+        getGraph().fafosp().vertical();
 
-        assertForEachNode(new int[] {1, 1}, getUnscaledYPosition);
+        assertForEachNode(new int[]{1, 1}, getUnscaledYPosition);
     }
 
     /**
@@ -61,11 +61,11 @@ class NewFafospYTest extends GraphBasedTest {
     @Test
     void testThreeSequential() {
         createGraph(3);
-        addEdges(new int[][] {{0, 1}, {1, 2}});
+        addEdges(new int[][]{{0, 1}, {1, 2}});
 
-        getGraph().fafospY();
+        getGraph().fafosp().vertical();
 
-        assertForEachNode(new int[] {1, 1, 1}, getUnscaledYPosition);
+        assertForEachNode(new int[]{1, 1, 1}, getUnscaledYPosition);
     }
 
     /**
@@ -80,11 +80,11 @@ class NewFafospYTest extends GraphBasedTest {
     @Test
     void testTwoParallel() {
         createGraph(4);
-        addEdges(new int[][] {{0, 1}, {0, 2}, {1, 3}, {2, 3}});
+        addEdges(new int[][]{{0, 1}, {0, 2}, {1, 3}, {2, 3}});
 
-        getGraph().fafospY();
+        getGraph().fafosp().vertical();
 
-        assertForEachNode(new int[] {2, 1, 3, 2}, getUnscaledYPosition);
+        assertForEachNode(new int[]{2, 1, 3, 2}, getUnscaledYPosition);
     }
 
     /**
@@ -99,11 +99,11 @@ class NewFafospYTest extends GraphBasedTest {
     @Test
     void testThreeParallel() {
         createGraph(5);
-        addEdges(new int[][] {{0, 1}, {0, 2}, {0, 3}, {1, 4}, {2, 4}, {3, 4}});
+        addEdges(new int[][]{{0, 1}, {0, 2}, {0, 3}, {1, 4}, {2, 4}, {3, 4}});
 
-        getGraph().fafospY();
+        getGraph().fafosp().vertical();
 
-        assertForEachNode(new int[] {3, 1, 3, 5, 3}, getUnscaledYPosition);
+        assertForEachNode(new int[]{3, 1, 3, 5, 3}, getUnscaledYPosition);
     }
 
     /**
@@ -118,41 +118,41 @@ class NewFafospYTest extends GraphBasedTest {
     @Test
     void testTwoParallelTwice() {
         createGraph(7);
-        addEdges(new int[][] {{0, 1}, {0, 2}, {1, 3}, {2, 3}, {3, 4}, {3, 5}, {4, 6}, {5, 6}});
+        addEdges(new int[][]{{0, 1}, {0, 2}, {1, 3}, {2, 3}, {3, 4}, {3, 5}, {4, 6}, {5, 6}});
 
-        getGraph().fafospY();
+        getGraph().fafosp().vertical();
 
-        assertForEachNode(new int[] {2, 1, 3, 2, 1, 3, 2}, getUnscaledYPosition);
+        assertForEachNode(new int[]{2, 1, 3, 2, 1, 3, 2}, getUnscaledYPosition);
     }
 
     /**
-     * . . . . . . . . . . . . .
-     * . . . . . . . 3 . . . . .
-     * . . . . . . / . \ . . . .
-     * . . . . . 1 . . . \ . . .
-     * . . . . / . \ . . . \ . .
-     * . . . / . . . 4 - - - | .
-     * . . / . . . . . . . . | .
-     * . 0 . . . . . . . . . 7 .
-     * . . \ . . . . . . . . | .
-     * . . . \ . . . 5 - - - | .
-     * . . . . \ . / . . . / . .
-     * . . . . . 2 . . . / . . .
-     * . . . . . . \ . / . . . .
-     * . . . . . . . 6 . . . . .
-     * . . . . . . . . . . . . .
+     * . . . . . . . . . . . . . . .
+     * . . . . . . . 3 . . . . . . .
+     * . . . . . . / . \ . . . . . .
+     * . . . . . 1 . . . \ . . . . .
+     * . . . . / . \ . . . \ . . . .
+     * . . . / . . . 4 - - - \ . . .
+     * . . / . . . . . . . . . \ . .
+     * . 0 . . . . . . . . . . . 7 .
+     * . . \ . . . . . . . . . / . .
+     * . . . \ . . . 5 - - - / . . .
+     * . . . . \ . / . . . / . . . .
+     * . . . . . 2 . . . / . . . . .
+     * . . . . . . \ . / . . . . . .
+     * . . . . . . . 6 . . . . . . .
+     * . . . . . . . . . . . . . . .
      */
     @Test
     void testFafospDoubleTwoSplit() {
         createGraph(8);
-        addEdges(new int[][] {
+        addEdges(new int[][]{
                 {0, 1}, {0, 2}, {1, 3}, {1, 4}, {2, 5},
                 {2, 6}, {3, 7}, {4, 7}, {5, 7}, {6, 7}
         });
 
-        getGraph().fafospY();
+        getGraph().fafosp().vertical();
 
-        assertForEachNode(new int[] {4, 2, 6, 1, 3, 5, 7, 4}, getUnscaledYPosition);
+        assertForEachNode(new int[]{4, 2, 6, 1, 3, 5, 7, 4}, getUnscaledYPosition);
     }
 
     /**
@@ -175,14 +175,14 @@ class NewFafospYTest extends GraphBasedTest {
     @Test
     void testFafospBidirectionalDoubleTwoSplit() {
         createGraph(10);
-        addEdges(new int[][] {
+        addEdges(new int[][]{
                 {0, 1}, {0, 2}, {1, 3}, {1, 4}, {2, 5}, {2, 6},
                 {3, 7}, {4, 7}, {5, 8}, {6, 8}, {7, 9}, {8, 9}
         });
 
-        getGraph().fafospY();
+        getGraph().fafosp().vertical();
 
-        assertForEachNode(new int[] {4, 2, 6, 1, 3, 5, 7, 2, 6, 4}, getUnscaledYPosition);
+        assertForEachNode(new int[]{4, 2, 6, 1, 3, 5, 7, 2, 6, 4}, getUnscaledYPosition);
     }
 
     /**
@@ -201,11 +201,11 @@ class NewFafospYTest extends GraphBasedTest {
     @Test
     void testFafospSharedDoubleTwoSplit() {
         createGraph(7);
-        addEdges(new int[][] {{0, 1}, {0, 2}, {1, 3}, {1, 4}, {2, 4}, {2, 5}, {3, 6}, {4, 6}, {5, 6}});
+        addEdges(new int[][]{{0, 1}, {0, 2}, {1, 3}, {1, 4}, {2, 4}, {2, 5}, {3, 6}, {4, 6}, {5, 6}});
 
-        getGraph().fafospY();
+        getGraph().fafosp().vertical();
 
-        assertForEachNode(new int[] {4, 2, 6, 1, 4, 7, 4}, getUnscaledYPosition);
+        assertForEachNode(new int[]{4, 2, 6, 1, 4, 7, 4}, getUnscaledYPosition);
     }
 
     /**
@@ -222,10 +222,10 @@ class NewFafospYTest extends GraphBasedTest {
     @Test
     void testFafospDoubleReversedSplit() {
         createGraph(6);
-        addEdges(new int[][] {{0, 1}, {0, 2}, {1, 3}, {2, 3}, {2, 4}, {3, 5}, {4, 5}});
+        addEdges(new int[][]{{0, 1}, {0, 2}, {1, 3}, {2, 3}, {2, 4}, {3, 5}, {4, 5}});
 
-        getGraph().fafospY();
+        getGraph().fafosp().vertical();
 
-        assertForEachNode(new int[] {3, 1, 4, 2, 5, 3}, getUnscaledYPosition);
+        assertForEachNode(new int[]{3, 1, 4, 2, 5, 3}, getUnscaledYPosition);
     }
 }
