b99a6b2cd90e03921b8e590eb6a1b018eb76ce6e
Georgios Andreadis <g.andreadis@student.tudelft.nl>
2017-05-24 12:03:30 +0200

    Add node iteration draft to coord. indexing
    (cherry picked from commit 2d6cdb7)

--- src/main/java/org/dnacronym/hygene/coordinatesystem/CoordinateIndexParser.java
+++ src/main/java/org/dnacronym/hygene/coordinatesystem/CoordinateIndexParser.java
@@ -1,5 +1,9 @@
 package org.dnacronym.hygene.coordinatesystem;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.dnacronym.hygene.models.GraphIterator;
+import org.dnacronym.hygene.models.SequenceDirection;
 import org.dnacronym.hygene.parser.GfaFile;
 import org.dnacronym.hygene.parser.MetadataParser;
 import org.dnacronym.hygene.parser.ParseException;
@@ -7,14 +11,18 @@ import org.dnacronym.hygene.parser.ParseException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
 
 
 /**
- * Created by gandr on 5/23/2017.
+ * Class responsible for indexing the genome coordinate system of a file.
  */
-public class CoordinateIndexParser {
+public final class CoordinateIndexParser {
+    static final int BASE_CACHE_INTERVAL = 1000;
+
+    private static final Logger LOGGER = LogManager.getLogger(GfaFile.class);
 
     private GfaFile gfaFile;
     private Map<String, Integer> genomeBaseCounts;
@@ -26,24 +34,54 @@ public class CoordinateIndexParser {
     }
 
     void parse() throws ParseException {
-        // Get new buffered reader
-        final BufferedReader gfa = gfaFile.readFile();
+        loadGenomeList();
+
+        final GraphIterator graphIterator = new GraphIterator(gfaFile.getGraph());
+        graphIterator.visitAll(SequenceDirection.RIGHT, nodeId -> {
+            try {
+                final List<String> nodeGenomes = gfaFile.getGraph().getNode(nodeId).retrieveMetadata().getGenomes();
+
+                for (final String genome : nodeGenomes) {
+                    final Integer previousBaseCount = genomeBaseCounts.get(genome);
+
+                    if (previousBaseCount == null) {
+                        throw new ParseException("Unrecognized genome found at node " + nodeId + ".");
+                    }
+                }
+            } catch (final ParseException e) {
+                LOGGER.warn("Failed to read metadata of node " + nodeId + ".");
+            }
+        });
+    }
+
+    /**
+     * Retrieves the list of genomes in the GFA file from the header.
+     *
+     * @throws ParseException in case of errors during parsing of the GFA file
+     */
+    private void loadGenomeList() throws ParseException {
+        final BufferedReader bufferedReader = gfaFile.readFile();
 
         try {
             String line;
-            while ((line = gfa.readLine()) != null && line.startsWith("H")) {
-                parseHeader(line);
+            while ((line = bufferedReader.readLine()) != null && line.startsWith("H")) {
+                parseHeaderLine(line);
             }
         } catch (final IOException e) {
             throw new ParseException("An error while reading the GFA file.", e);
         }
     }
 
-    private void parseHeader(final String line) throws ParseException {
+    /**
+     * Parses one header line and checks for genome metadata.
+     *
+     * @param line the line to parse
+     * @throws ParseException in case of errors during parsing of the GFA file
+     */
+    private void parseHeaderLine(final String line) throws ParseException {
         final StringTokenizer stringTokenizer = new StringTokenizer(line, "\t");
 
         stringTokenizer.nextToken(); // Ignore "H" token
-        // Ignore headers not containing relevant genome information
         final String headerBody = stringTokenizer.nextToken();
         if (!headerBody.startsWith(MetadataParser.GENOME_LIST_HEADER_PREFIX)) {
             return;
