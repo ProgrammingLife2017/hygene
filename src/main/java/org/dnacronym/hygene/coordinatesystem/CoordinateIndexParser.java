2c68fe8128c0ae4a3b86e8991420763c958c9a9b
Georgios Andreadis <g.andreadis@student.tudelft.nl>
2017-05-24 10:46:40 +0200

    Let CoordinateIndexParser only parse header
    (cherry picked from commit aea0b20)

--- src/main/java/org/dnacronym/hygene/coordinatesystem/CoordinateIndexParser.java
+++ src/main/java/org/dnacronym/hygene/coordinatesystem/CoordinateIndexParser.java
@@ -4,6 +4,8 @@ import org.dnacronym.hygene.parser.GfaFile;
 import org.dnacronym.hygene.parser.MetadataParser;
 import org.dnacronym.hygene.parser.ParseException;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
@@ -23,38 +25,25 @@ public class CoordinateIndexParser {
         this.genomeBaseCounts = new HashMap<>();
     }
 
-    void parse() {
+    void parse() throws ParseException {
+        // Get new buffered reader
+        final BufferedReader gfa = gfaFile.readFile();
 
-    }
-
-    private void parseLine(final String line) throws ParseException {
-        final StringTokenizer st = new StringTokenizer(line, "\t");
-        if (!st.hasMoreTokens()) {
-            return;
-        }
-
-        final String recordType = st.nextToken();
-        switch (recordType) {
-            case "L":
-            case "C":
-            case "P":
-                break;
-
-            case "H":
-                parseHeader(st);
-                break;
-
-            case "S":
-                break;
+        try {
+            String line;
+            while ((line = gfa.readLine()) != null && line.startsWith("H")) {
+                parseHeader(line);
+            }
+        } catch (final IOException e) {
+            throw new ParseException("An error while reading the GFA file.", e);
         }
     }
 
-    private void parseHeader(final StringTokenizer stringTokenizer) throws ParseException {
+    private void parseHeader(final String line) throws ParseException {
+        final StringTokenizer stringTokenizer = new StringTokenizer(line, "\t");
+
+        stringTokenizer.nextToken(); // Ignore "H" token
         // Ignore headers not containing relevant genome information
-        if (!stringTokenizer.hasMoreTokens()) {
-            return;
-        }
-
         final String headerBody = stringTokenizer.nextToken();
         if (!headerBody.startsWith(MetadataParser.GENOME_LIST_HEADER_PREFIX)) {
             return;
@@ -73,35 +62,6 @@ public class CoordinateIndexParser {
 
         if (genomeBaseCounts.isEmpty()) {
             throw new ParseException("Expected at least one genome to be present in GFA file.");
-        }
-    }
-
-    private void parseSegment(final StringTokenizer stringTokenizer) throws ParseException {
-        final String nodeName = stringTokenizer.nextToken();
-
-        stringTokenizer.nextToken(); // Ignore base sequence
-        stringTokenizer.nextToken(); // Ignore asterisk
-
-        final String genomeMetadata = stringTokenizer.nextToken();
-        if (!genomeMetadata.startsWith(MetadataParser.GENOME_LIST_HEADER_PREFIX)) {
-            return;
-        }
-
-        final String genomeListString = genomeMetadata.substring(MetadataParser.GENOME_LIST_HEADER_PREFIX.length());
-        final StringTokenizer bodyTokenizer = new StringTokenizer(genomeListString, ";");
-
-        while (bodyTokenizer.hasMoreTokens()) {
-            final String nextGenome = bodyTokenizer.nextToken();
-
-            if (nextGenome.equals("")) {
-                continue;
-            }
-
-            if (!genomeBaseCounts.containsKey(nextGenome)) {
-                throw new ParseException("Unkown genome '" + nextGenome + "' on segment " + nodeName);
-            }
-
-            final int previousDistance = genomeBaseCounts.get(nextGenome);
         }
     }
 }
