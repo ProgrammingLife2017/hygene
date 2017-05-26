8a88c76e8a2f886737e10e579104e0dd0d56ba88
Joel Abrahams <abrahamsjo@gmail.com>
2017-05-26 15:03:43 +0200

    Fix formatting of Test class

--- src/test/java/org/dnacronym/hygene/ui/controller/leftpane/NodePropertiesControllerTest.java
+++ src/test/java/org/dnacronym/hygene/ui/controller/leftpane/NodePropertiesControllerTest.java
@@ -14,6 +14,7 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+
 /**
  * Unit tests for {@link NodePropertiesController}s.
  */
