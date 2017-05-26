b3e0726b114806a89ee4063cef31ca3b5cacbe32
Joel Abrahams <abrahamsjo@gmail.com>
2017-05-26 17:06:06 +0200

    Add view of creating bookmarks

---
+++ src/main/java/org/dnacronym/hygene/ui/controller/bookmarks/CreateBookmarkController.java
@@ -0,0 +1,17 @@
+package org.dnacronym.hygene.ui.controller.bookmarks;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+/**
+ * Controller for creating {@link org.dnacronym.hygene.models.Bookmark}s.
+ */
+public class CreateBookmarkController implements Initializable {
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+}
