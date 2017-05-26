45f237ff94c403ec687039cd1e3be953f058fdb4
Georgios Andreadis <g.andreadis@student.tudelft.nl>
2017-05-26 13:05:52 +0200

    Add javadocs
    (cherry picked from commit 98cf597)

--- src/main/java/org/dnacronym/hygene/coordinatesystem/CoordinateSystemIndex.java
+++ src/main/java/org/dnacronym/hygene/coordinatesystem/CoordinateSystemIndex.java
@@ -7,6 +7,7 @@ import org.dnacronym.hygene.models.SequenceDirection;
 import org.dnacronym.hygene.parser.GfaFile;
 import org.dnacronym.hygene.parser.MetadataParser;
 import org.dnacronym.hygene.parser.ParseException;
+import org.dnacronym.hygene.persistence.FileGenomeIndex;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -26,19 +27,32 @@ public final class CoordinateSystemIndex {
     private static final Logger LOGGER = LogManager.getLogger(GfaFile.class);
 
     private final GfaFile gfaFile;
+    private final FileGenomeIndex fileGenomeIndex;
     private final Map<String, Integer> genomeBaseCounts;
     private final Map<String, Integer> genomeBaseDiffCounts;
     private final List<String> genomeNames;
 
 
-    public CoordinateSystemIndex(GfaFile gfaFile) {
+    /**
+     * Constructs a new {@link CoordinateSystemIndex} instance.
+     *
+     * @param gfaFile         the graph file to index
+     * @param fileGenomeIndex the {@link FileGenomeIndex} instance to be used for storing and retrieving data
+     */
+    public CoordinateSystemIndex(final GfaFile gfaFile, FileGenomeIndex fileGenomeIndex) {
         this.gfaFile = gfaFile;
+        this.fileGenomeIndex = fileGenomeIndex;
         this.genomeBaseCounts = new HashMap<>();
         this.genomeBaseDiffCounts = new HashMap<>();
         this.genomeNames = new ArrayList<>();
     }
 
 
+    /**
+     * Populate the file index with genome coordinate system index points.
+     *
+     * @throws ParseException in case of errors during parsing of the GFA file
+     */
     void populateIndex() throws ParseException {
         loadGenomeList();
         saveIndexPoints();
@@ -85,7 +99,7 @@ public final class CoordinateSystemIndex {
 
             if (!nextGenome.equals("")) {
                 genomeBaseCounts.put(nextGenome, 0);
-                genomeBaseDiffCounts.put(nextGenome, 0);
+                genomeBaseDiffCounts.put(nextGenome, -BASE_CACHE_INTERVAL);
                 genomeNames.add(nextGenome);
             }
         }
@@ -95,6 +109,9 @@ public final class CoordinateSystemIndex {
         }
     }
 
+    /**
+     * Iterates through the graph and saves index points at selected base locations.
+     */
     private void saveIndexPoints() {
         final GraphIterator graphIterator = new GraphIterator(gfaFile.getGraph());
         graphIterator.visitAll(SequenceDirection.RIGHT, nodeId -> {
