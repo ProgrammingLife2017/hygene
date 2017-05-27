613d1393bad7582785ad9fcf6c7d2bec72ee2600
Joel Abrahams <abrahamsjo@gmail.com>
2017-05-28 00:00:24 +0200

    Fix help package info

--- src/main/java/org/dnacronym/hygene/ui/help/package-info.java
+++ src/main/java/org/dnacronym/hygene/ui/help/package-info.java
@@ -4,7 +4,7 @@
  * <ul>
  * <li> {@link org.dnacronym.hygene.ui.help.HelpArticle} contains the data for displaying the help article.
  * <li> {@link org.dnacronym.hygene.ui.help.HelpArticleParser} parses the xml file that contains the help information.
- * <li> {@link org.dnacronym.hygene.ui.help.HelpMenuController} controller that deals user input.
+ * <li> {@link org.dnacronym.hygene.ui.help.HelpMenuController} controller that deals with user input.
  * <li> {@link org.dnacronym.hygene.ui.help.HelpMenuView} view wrapper class that creates a new help view.
  * </ul>
  **/
