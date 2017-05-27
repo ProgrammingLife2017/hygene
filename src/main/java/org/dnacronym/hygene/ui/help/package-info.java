c6baf56046baf94e414943e9da8e5ff8f77c0dca
Joel Abrahams <abrahamsjo@gmail.com>
2017-05-27 19:28:43 +0200

    Improve javadoc help package package-info

--- src/main/java/org/dnacronym/hygene/ui/help/package-info.java
+++ src/main/java/org/dnacronym/hygene/ui/help/package-info.java
@@ -2,7 +2,7 @@
  * Everything to do with the help menu.
  * <p>
  * <ul>
- * <li> {@link org.dnacronym.hygene.ui.help.HelpArticle} displays with the help article.
+ * <li> {@link org.dnacronym.hygene.ui.help.HelpArticle} contains the data for displaying the help article.
  * <li> {@link org.dnacronym.hygene.ui.help.HelpArticleParser} parses the xml file that contains the help information.
  * <li> {@link org.dnacronym.hygene.ui.help.HelpMenuController} controller that deals user input.
  * <li> {@link org.dnacronym.hygene.ui.help.HelpMenuView} view wrapper class that creates a new help view.
