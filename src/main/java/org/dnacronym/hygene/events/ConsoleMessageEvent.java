1dc4e7e2ea652efefcf9affaaf00b01f68dc1b32
Niels de Bruin <nielsdebruin@gmail.com>
2017-06-02 09:48:36 +0200

    Create console message event

---
+++ src/main/java/org/dnacronym/hygene/events/ConsoleMessageEvent.java
@@ -0,0 +1,20 @@
+package org.dnacronym.hygene.events;
+
+import org.dnacronym.hygene.ui.console.ConsoleMessage;
+
+
+/**
+ * This event represent the creation of a new {@link ConsoleMessage}.
+ */
+public final class ConsoleMessageEvent {
+    private final ConsoleMessage consoleMessage;
+
+    /**
+     * Instantiates a new Console Message Event.
+     *
+     * @param consoleMessage the {@link ConsoleMessage}
+     */
+    public ConsoleMessageEvent(final ConsoleMessage consoleMessage) {
+        this.consoleMessage = consoleMessage;
+    }
+}
