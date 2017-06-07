b72793000ae32ec0164a3ee3ef72f78b0cf3995e
Georgios Andreadis <g.andreadis@student.tudelft.nl>
2017-06-07 13:28:57 +0200

    Refactor Query class

--- src/main/java/org/dnacronym/hygene/graph/Query.java
+++ src/main/java/org/dnacronym/hygene/graph/Query.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.function.Predicate;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -31,11 +32,25 @@ public final class Query {
      *
      * @param gfaFile the file to be queried
      */
-    public Query(GfaFile gfaFile) {
+    public Query(final GfaFile gfaFile) {
         this.gfaFile = gfaFile;
         this.numNodesInGraph = gfaFile.getGraph().getNodeArrays().length;
     }
 
+
+    /**
+     * Executes the given regex query on all node names.
+     *
+     * @param regex the regex to test against
+     * @return the IDs of nodes with names matching the regex
+     * @throws ParseException if the GFA file is invalid in some form
+     */
+    public Set<Integer> executeNameRegexQuery(final String regex) throws ParseException {
+        return executeQuery(nodeMetadata -> {
+            final Pattern pattern = Pattern.compile(regex);
+            return pattern.matcher(nodeMetadata.getName()).matches();
+        });
+    }
 
     /**
      * Executes a query on the GFA file.
@@ -46,33 +61,39 @@ public final class Query {
      */
     public Set<Integer> executeQuery(final Predicate<NodeMetadata> isInQuery) throws ParseException {
         final MetadataParser metadataParser = new MetadataParser();
-
         final int numBatches = numNodesInGraph / BATCH_SIZE;
         final Set<Integer> nodeIds = new HashSet<>();
 
-        for (int batch = 0; batch < numBatches; batch++) {
-            final List<Integer> batchNodeIds = IntStream.rangeClosed(
-                    batch * BATCH_SIZE + 1,
-                    Math.max(batch * (BATCH_SIZE + 1), numNodesInGraph - 2))
-                    .boxed().collect(Collectors.toList());
-
-            final Map<Integer, Integer> sortedNodeIds = batchNodeIds.stream()
-                    .sorted(Comparator.comparingInt(nodeId -> gfaFile.getGraph().getLineNumber(nodeId)))
-                    .collect(Collectors.toMap(
-                            nodeId -> nodeId,
-                            nodeId -> gfaFile.getGraph().getLineNumber(nodeId),
-                            (oldValue, newValue) -> oldValue,
-                            LinkedHashMap::new
-                    ));
+        for (int batchIndex = 0; batchIndex < numBatches; batchIndex++) {
+            final Map<Integer, Integer> sortedNodeIds = getLineNumbersOfBatch(batchIndex);
             final Map<Integer, NodeMetadata> batchMetadata = metadataParser.parseNodeMetadata(gfaFile, sortedNodeIds);
-            nodeIds.addAll(
-                    batchMetadata.entrySet().stream()
-                            .filter(entry -> isInQuery.test(entry.getValue()))
-                            .map(Map.Entry::getKey)
-                            .collect(Collectors.toSet())
-            );
+            nodeIds.addAll(batchMetadata.entrySet().stream()
+                    .filter(entry -> isInQuery.test(entry.getValue()))
+                    .map(Map.Entry::getKey)
+                    .collect(Collectors.toSet()));
         }
 
         return nodeIds;
+    }
+
+    /**
+     * Returns a map of node IDs to line numbers, sorted by line number.
+     *
+     * @param batchIndex the index of the batch to generate mappings for
+     * @return the sorted map of node IDs to line numbers
+     */
+    private Map<Integer, Integer> getLineNumbersOfBatch(final int batchIndex) {
+        final List<Integer> batchNodeIds = IntStream.rangeClosed(
+                batchIndex * BATCH_SIZE + 1, Math.max(batchIndex * (BATCH_SIZE + 1), numNodesInGraph - 2)
+        ).boxed().collect(Collectors.toList());
+
+        return batchNodeIds.stream()
+                .sorted(Comparator.comparingInt(nodeId -> gfaFile.getGraph().getLineNumber(nodeId)))
+                .collect(Collectors.toMap(
+                        nodeId -> nodeId,
+                        nodeId -> gfaFile.getGraph().getLineNumber(nodeId),
+                        (oldValue, newValue) -> oldValue,
+                        LinkedHashMap::new
+                ));
     }
 }
