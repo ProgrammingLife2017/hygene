a4a3a365fe5b28815f83053c3fd076e1079cb5bc
Felix Dekker <felix@fwdekker.com>
2017-06-07 11:34:33 +0200

    Implement Sugiyama half-way

--- src/main/java/org/dnacronym/hygene/graph/layout/FafospLayerer.java
+++ src/main/java/org/dnacronym/hygene/graph/layout/FafospLayerer.java
@@ -3,6 +3,8 @@ package org.dnacronym.hygene.graph.layout;
 import org.dnacronym.hygene.graph.NewNode;
 import org.dnacronym.hygene.graph.Subgraph;
 
+import java.util.Collection;
+
 
 /**
  * Part of the {@link SugiyamaLayout} algorithm, layers a {@link Subgraph} into a number of layers using the FAFOSP-X
@@ -26,15 +28,50 @@ public final class FafospLayerer implements SugiyamaLayerer {
             return null;
         }
 
+        final Collection<NewNode> nodes = subgraph.getNodes();
+        final NewNode[][] layers = getLayers(nodes);
+
+        nodes.forEach(node -> {
+            //
+        });
+
         return new NewNode[0][];
     }
 
 
     //
-    private int getLayerCount(final Subgraph subgraph) {
-        final int maxPosition = subgraph.getNodes().stream().map(NewNode::getXPosition).max(Integer::compare).get();
-        return positionToLayer(maxPosition);
+    private NewNode[][] getLayers(final Collection<NewNode> nodes) {
+        final int[] layerHeights = getHeights(nodes);
+        final NewNode[][] layers = new NewNode[layerHeights.length][];
+
+        for (int i = 0; i < layers.length; i++) {
+            layers[i] = new NewNode[layerHeights[i]];
+        }
+
+        return layers;
     }
+
+    //
+    private void addToLayers(final Collection<NewNode> nodes, final NewNode[][] layers) {
+        nodes.forEach(node -> {
+            final int nodeStartLayer = positionToLayer(node.getXPosition());
+            final int nodeEndLayer = positionToLayer(node.getXPosition() + node.getLength());
+
+            for (int layer = nodeStartLayer; layer < nodeEndLayer; layer++) {
+                addToLayerSomewhere(layers[layer], node);
+            }
+
+            node.getOutgoingEdges().forEach(edge -> {
+                final NewNode neighbour = edge.getTo();
+                final int neighbourStartLayer = positionToLayer(neighbour.getXPosition());
+
+                for (int layer = nodeEndLayer + 1; layer < neighbourStartLayer; layer++) {
+                    addToLayerSomewhere(layers[layer], neighbour);
+                }
+            });
+        });
+    }
+
 
     //
     private int positionToLayer(final int position) {
@@ -44,5 +81,49 @@ public final class FafospLayerer implements SugiyamaLayerer {
     //
     private int layerToPosition(final int column) {
         return column * LAYER_WIDTH;
+    }
+
+    //
+    private int getLayerCount(final Collection<NewNode> nodes) {
+        final int maxPosition = nodes.stream().map(NewNode::getXPosition).max(Integer::compare).get();
+        return positionToLayer(maxPosition);
+    }
+
+    //
+    private int[] getHeights(final Collection<NewNode> nodes) {
+        final int layerCount = getLayerCount(nodes);
+        final int[] heights = new int[layerCount];
+
+        nodes.forEach(node -> {
+            final int nodeStartLayer = positionToLayer(node.getXPosition());
+            final int nodeEndLayer = positionToLayer(node.getXPosition() + node.getLength());
+
+            for (int layer = nodeStartLayer; layer < nodeEndLayer; layer++) {
+                heights[layer]++;
+            }
+
+            node.getOutgoingEdges().forEach(edge -> {
+                final NewNode neighbour = edge.getTo();
+                final int neighbourStartLayer = positionToLayer(neighbour.getXPosition());
+
+                for (int layer = nodeEndLayer + 1; layer < neighbourStartLayer; layer++) {
+                    heights[layer]++;
+                }
+            });
+        });
+
+        return heights;
+    }
+
+    //
+    private void addToLayerSomewhere(final NewNode[] layer, final NewNode value) {
+        for (int i = 0; i < layer.length; i++) {
+            if (layer[i] == null) {
+                layer[i] = value;
+                return;
+            }
+        }
+
+        throw new IllegalStateException("");
     }
 }
