4a3e83c08acea6d4135e9937eb0f060f387f75a0
Georgios Andreadis <g.andreadis@student.tudelft.nl>
2017-06-07 14:43:19 +0200

    Clean up query test properly

--- src/test/java/org/dnacronym/hygene/graph/SearchQueryTest.java
+++ src/test/java/org/dnacronym/hygene/graph/SearchQueryTest.java
@@ -3,8 +3,16 @@ package org.dnacronym.hygene.graph;
 import org.dnacronym.hygene.parser.GfaFile;
 import org.dnacronym.hygene.parser.ParseException;
 import org.dnacronym.hygene.parser.ProgressUpdater;
+import org.dnacronym.hygene.parser.factories.MetadataParserFactory;
+import org.dnacronym.hygene.parser.factories.NewGfaParserFactory;
+import org.dnacronym.hygene.persistence.FileDatabaseDriver;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,6 +31,14 @@ class SearchQueryTest {
         GfaFile gfaFile = new GfaFile(GFA_TEST_FILE);
         gfaFile.parse(ProgressUpdater.DUMMY);
         searchQuery = new SearchQuery(gfaFile);
+    }
+
+    @AfterEach
+    void afterEach() throws IOException {
+        NewGfaParserFactory.setInstance(null);
+        MetadataParserFactory.setInstance(null);
+        Files.deleteIfExists(Paths.get(GFA_TEST_FILE + FileDatabaseDriver.DB_FILE_EXTENSION));
+        Files.deleteIfExists(Paths.get(GFA_TEST_FILE + ".hygenecache"));
     }
 
 
