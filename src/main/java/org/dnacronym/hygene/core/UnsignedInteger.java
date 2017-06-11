f73e1e26d0999838b20ec35de7380839b8b703d2
Casper Boone <mail@casperboone.nl>
2017-06-12 01:18:48 +0200

    Add unsigned integer

---
+++ src/main/java/org/dnacronym/hygene/core/UnsignedInteger.java
@@ -0,0 +1,46 @@
+package org.dnacronym.hygene.core;
+
+
+/**
+ * Represents an 32 bit unsigned integer, allowing to store numbers between 0 and 2^32.
+ */
+public final class UnsignedInteger {
+    /**
+     * The maximum value of an {@link UnsignedInteger}, 2 * max integer value + 1.
+     */
+    public static final long MAX_VALUE = 4294967295L;
+
+
+    /**
+     * Hides public implicit constructor.
+     */
+    private UnsignedInteger() {
+    }
+
+
+    /**
+     * Converts a normal non negative long to a regularized integer.
+     *
+     * @param value a non negative long
+     * @return a regularized integer between {@code Integer.MIN_VALUE} and {@code Integer.MAX_VALUE}
+     */
+    public static int fromLong(final long value) {
+        if (value < 0) {
+            throw new IllegalArgumentException("A negative number cannot be converted to an unsigned integer.");
+        }
+        if (value > MAX_VALUE) {
+            throw new IllegalArgumentException("Number cannot be higher than " + MAX_VALUE + ".");
+        }
+        return (int) value + Integer.MIN_VALUE;
+    }
+
+    /**
+     * Converts a regularized integer to a non negative long.
+     *
+     * @param regularizedInteger a regularized integer between {@code Integer.MIN_VALUE} and {@code Integer.MAX_VALUE}
+     * @return a non negative long
+     */
+    public static long toLong(final int regularizedInteger) {
+        return (long) regularizedInteger - Integer.MIN_VALUE;
+    }
+}
---
+++ src/test/java/org/dnacronym/hygene/core/UnsignedIntegerTest.java
@@ -0,0 +1,71 @@
+package org.dnacronym.hygene.core;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+
+/**
+ * Unit tests for {@link UnsignedInteger}.
+ */
+public final class UnsignedIntegerTest {
+    @Test
+    void itConvertsALongToAnUnsignedInteger() {
+        assertThat(UnsignedInteger.fromLong(42L)).isEqualTo(Integer.MIN_VALUE + 42);
+    }
+
+    @Test
+    void itConvertsATheLowestAcceptedLongToAnUnsignedInteger() {
+        assertThat(UnsignedInteger.fromLong(0L)).isEqualTo(Integer.MIN_VALUE);
+    }
+
+    @Test
+    void itConvertsALongAboveTheIntegerBoundaryToAnUnsignedInteger() {
+        assertThat(UnsignedInteger.fromLong(Integer.MAX_VALUE + 1L)).isEqualTo(0);
+    }
+
+    @Test
+    void itConvertsTheHighestAcceptedLongToAnUnsignedInteger() {
+        assertThat(UnsignedInteger.fromLong(UnsignedInteger.MAX_VALUE)).isEqualTo(Integer.MAX_VALUE);
+    }
+
+    @Test
+    void itCannotConvertALongThatIsTooHigh() {
+        final long tooHighLong = UnsignedInteger.MAX_VALUE + 1L;
+        final Throwable throwable = catchThrowable(() -> UnsignedInteger.fromLong(tooHighLong));
+
+        assertThat(throwable).isInstanceOf(IllegalArgumentException.class);
+        assertThat(throwable).hasMessageContaining("Number cannot be higher than 4294967295.");
+    }
+
+    @Test
+    void itCannotConvertANegativeLongToAnUnsignedInteger() {
+        final Throwable throwable = catchThrowable(() -> UnsignedInteger.fromLong(-1L));
+
+        assertThat(throwable).isInstanceOf(IllegalArgumentException.class);
+        assertThat(throwable).hasMessageContaining("A negative number cannot be converted to an unsigned integer.");
+    }
+
+    @Test
+    void itConvertsAnUnsignedIntegerToALong() {
+        final int unsignedInteger = UnsignedInteger.fromLong(42L);
+
+        assertThat(UnsignedInteger.toLong(unsignedInteger)).isEqualTo(42L);
+    }
+
+    @Test
+    void itConvertsTheLowestUnsignedIntegerToALong() {
+        final int unsignedInteger = UnsignedInteger.fromLong(0L);
+
+        assertThat(UnsignedInteger.toLong(unsignedInteger)).isEqualTo(0L);
+    }
+
+    @Test
+    void itConvertsTheHighestUnsignedIntegerToALong() {
+        final int unsignedInteger = UnsignedInteger.fromLong(UnsignedInteger.MAX_VALUE);
+        System.out.println(unsignedInteger);
+
+        assertThat(UnsignedInteger.toLong(unsignedInteger)).isEqualTo(UnsignedInteger.MAX_VALUE);
+    }
+}
