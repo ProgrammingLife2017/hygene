52d0e74f03dc1f9df6ebf2e5544b727e85a659ab
Felix Dekker <felix@fwdekker.com>
2017-06-11 21:17:22 +0200

    Implement ThrottledRunnable

---
+++ src/main/java/org/dnacronym/hygene/core/ThrottledRunnable.java
@@ -0,0 +1,87 @@
+package org.dnacronym.hygene.core;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+
+/**
+ * A threaded {@link Runnable} that can only be called a limited number of times
+ * <p>
+ * When {@link #run()} is called and there is no thread currently running, the action is scheduled until a later
+ * moment if executing it now would violate the timeout between executions, or it is executed immediately otherwise.
+ * <p>
+ * When {@link #run()} is called while a thread is currently running, that thread is interrupted. If no action is
+ * scheduled, the action is scheduled until a later moment if executing it now would violate the timeout between
+ * executions, or it is executed immediately otherwise.
+ */
+public final class ThrottledRunnable {
+    private static final Logger LOGGER = LogManager.getLogger(ThrottledRunnable.class);
+
+    private final ScheduledExecutorService executor;
+    private final Runnable action;
+    private final int timeout;
+
+    private ScheduledFuture<?> future;
+    private Instant waitUntil;
+
+
+    /**
+     * Constructs a new {@link ThrottledRunnable}.
+     *
+     * @param action  an action to perform
+     * @param timeout the minimal time between each execution
+     */
+    public ThrottledRunnable(final Runnable action, final int timeout) {
+        if (timeout < 0) {
+            throw new IllegalArgumentException("The limit must be a positive integer.");
+        }
+
+        this.executor = Executors.newSingleThreadScheduledExecutor();
+        this.action = action;
+        this.timeout = timeout;
+
+        this.waitUntil = Instant.now();
+    }
+
+
+    /**
+     * Tries to execute the action in a separate thread, or schedules one if that is not possible.
+     */
+    public synchronized void run() {
+        if (future != null && !future.isDone() && !future.isCancelled()) {
+            if (future.getDelay(TimeUnit.MILLISECONDS) >= 0) {
+                return;
+            } else {
+                future.cancel(true);
+            }
+        }
+
+        final long delay = Math.max(0, Duration.between(Instant.now(), waitUntil).toMillis());
+        future = executor.schedule(action, delay, TimeUnit.MILLISECONDS);
+        waitUntil = Instant.now().plus(timeout, ChronoUnit.MILLIS);
+    }
+
+    /**
+     * Blocks the current thread until the action has completed.
+     */
+    public synchronized void block() {
+        if (future == null) {
+            return;
+        }
+
+        try {
+            future.get();
+        } catch (final InterruptedException | ExecutionException e) {
+            LOGGER.warn(e);
+        }
+    }
+}
---
+++ src/test/java/org/dnacronym/hygene/core/ThrottledRunnableTest.java
@@ -0,0 +1,156 @@
+package org.dnacronym.hygene.core;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+import java.time.Instant;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+
+/**
+ * Unit tests for {@link ThrottledRunnable}.
+ */
+class ThrottledRunnableTest {
+    /**
+     * Tests that a timeout of fewer than 0 milliseconds is not accepted.
+     */
+    @Test
+    void testIllegalTimeout() {
+        final Throwable throwable = catchThrowable(() -> new ThrottledRunnable(() -> {}, -24));
+
+        assertThat(throwable).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    /**
+     * Tests that the runnable is executed.
+     */
+    @Test
+    void testRuns() {
+        final int[] number = {0};
+        final ThrottledRunnable runnable = new ThrottledRunnable(() -> number[0]++, 34);
+
+        runnable.run();
+        runnable.block();
+
+        assertThat(number[0]).isEqualTo(1);
+    }
+
+    /**
+     * Tests that the runnable is executed twice.
+     */
+    @Test
+    void testRunsTwice() {
+        final int[] number = {0};
+        final ThrottledRunnable runnable = new ThrottledRunnable(() -> number[0]++, 55);
+
+        runnable.run();
+        runnable.block();
+        runnable.run();
+        runnable.block();
+
+        assertThat(number[0]).isEqualTo(2);
+    }
+
+    /**
+     * Tests that the second run has to wait for some time after the first run.
+     */
+    @Test
+    void testRunsTwiceDelay() {
+        final int[] number = {0};
+        final ThrottledRunnable runnable = new ThrottledRunnable(() -> number[0]++, 500);
+
+        runnable.run();
+        runnable.block();
+        final Instant then = Instant.now();
+        runnable.run();
+        runnable.block();
+        final long time = Duration.between(then, Instant.now()).toMillis();
+
+        assertThat(time).isGreaterThan(250);
+    }
+
+    /**
+     * Tests that the runnable is executed once, because the first run is interrupted by the second.
+     */
+    @Test
+    void testInterruptOnce() {
+        final int[] number = {0};
+        final ThrottledRunnable runnable = new ThrottledRunnable(() -> number[0]++, 0);
+
+        runnable.run();
+        runnable.run();
+        runnable.block();
+
+        assertThat(number[0]).isEqualTo(1);
+    }
+
+    /**
+     * Tests that the runnable is executed once, because the first run is interrupted by the second, and the second
+     * run is interrupted by the third.
+     */
+    @Test
+    void testInterruptTwice() {
+        final int[] number = {0};
+        final ThrottledRunnable runnable = new ThrottledRunnable(() -> number[0]++, 0);
+
+        runnable.run();
+        runnable.run();
+        runnable.run();
+        runnable.block();
+
+        assertThat(number[0]).isEqualTo(1);
+    }
+
+    /**
+     * Tests that the runnable is executed once, because the first run is interrupted by the second and the third run
+     * sees that the second run scheduled a run.
+     */
+    @Test
+    void testScheduled() {
+        final int[] number = {0};
+        final ThrottledRunnable runnable = new ThrottledRunnable(() -> {
+            number[0]++;
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, 500);
+
+        runnable.run();
+        runnable.run();
+        runnable.run();
+        runnable.block();
+
+        assertThat(number[0]).isEqualTo(1);
+    }
+
+    /**
+     * Tests that a block returns if {@link ThrottledRunnable#run()} is never called.
+     * <p>
+     * This method does not contain an assert because it tests that a method returns at all.
+     */
+    @Test
+    void testBlockWithoutRun() {
+        final ThrottledRunnable runnable = new ThrottledRunnable(() -> {}, 500);
+
+        runnable.block();
+    }
+
+    /**
+     * Tests that block returns if the runnable throws an exception.
+     * <p>
+     * This method does not contain an assert because it tests that a method returns at all.
+     */
+    @Test
+    void testBlockInterrupted() {
+        final ThrottledRunnable runnable = new ThrottledRunnable(() -> {
+            throw new RuntimeException("Dummy exception");
+        }, 500);
+
+        runnable.run();
+        runnable.block();
+    }
+}
