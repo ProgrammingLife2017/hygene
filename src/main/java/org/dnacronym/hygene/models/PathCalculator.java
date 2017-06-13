27a5fea9a109110bc9dc477fb85b8dd29085ce67
Niels de Bruin <nielsdebruin@gmail.com>
2017-06-13 23:24:29 +0200

    Split compute paths in several methods.

--- src/main/java/org/dnacronym/hygene/models/PathCalculator.java
+++ src/main/java/org/dnacronym/hygene/models/PathCalculator.java
@@ -21,29 +21,24 @@ import java.util.stream.Collectors;
 /**
  * The class with calculate the genome paths.
  */
-public class PathCalculator {
+public final class PathCalculator {
     private static final Logger LOGGER = LogManager.getLogger(PathCalculator.class);
 
     private Subgraph subgraph;
+    private Map<NewNode, Set<String>> genomeStore;
 
-    private Set<Edge> visited;
 
-
-    public PathCalculator(Subgraph subgraph) {
+    public PathCalculator(final Subgraph subgraph) {
         this.subgraph = subgraph;
-        this.visited = new HashSet<>();
+        this.genomeStore = new HashMap<>();
     }
 
 
     public void computePaths(final Subgraph subgraph) {
+        genomeStore.clear();
+
         // Determine start
-        List<NewNode> sourceConnectedNodes = subgraph.getNodes().stream()
-                .filter(n -> subgraph.getNeighbours(n, SequenceDirection.LEFT).isEmpty())
-                .collect(Collectors.toList());
-
-        // Compute topological ordering
-
-        Map<NewNode, Set<String>> genomeStore = new HashMap<>();
+        List<NewNode> sourceConnectedNodes = getNodesWithNoIncomingEdges(subgraph);
 
         final Queue<Edge> toVisit = new LinkedList<>();
 
@@ -88,6 +83,36 @@ public class PathCalculator {
 
         System.out.println(genomeStore);
 
+        // Generate paths
+        Map<Edge, Set<String>> paths = topologicalPathGeneration(topologicalOrder);
+
+        // Add paths to edges
+        addPathsToEdges(paths);
+        System.out.println(paths);
+    }
+
+
+    /**
+     * Determines which {@link NewNode}s do not have any incoming edges.
+     * <p>
+     * These {@link NewNode}s are considered to be connected the theoretical source node of the Graph.
+     *
+     * @param subgraph the subgraph
+     * @return list of nodes with no incoming edges.
+     */
+    public List<NewNode> getNodesWithNoIncomingEdges(final Subgraph subgraph) {
+        return subgraph.getNodes().stream()
+                .filter(n -> subgraph.getNeighbours(n, SequenceDirection.LEFT).isEmpty())
+                .collect(Collectors.toList());
+    }
+
+    /**
+     * Uses a topologically ordered set of {@link NewNode}s to compute the edges paths.
+     *
+     * @param topologicalOrder the topological ordering
+     * @return map contains a set of genomes for each edge
+     */
+    public Map<Edge, Set<String>> topologicalPathGeneration(final List<NewNode> topologicalOrder) {
         // Create edges genome store
         Map<Edge, Set<String>> paths = new HashMap<>();
 
@@ -103,6 +128,15 @@ public class PathCalculator {
             });
         });
 
-        System.out.println(paths);
+        return paths;
+    }
+
+    /**
+     * Will add a set of computed path to its corresponding {@link Edge}.
+     *
+     * @param paths the paths
+     */
+    public void addPathsToEdges(final Map<Edge, Set<String>> paths) {
+        paths.forEach(Edge::setGenomes);
     }
 }
