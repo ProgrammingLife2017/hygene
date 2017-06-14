cd05e97a877103dfa0adfbbe6648045b8da0ec48
Georgios Andreadis <g.andreadis@student.tudelft.nl>
2017-06-14 10:49:28 +0200

    Bump database version

--- src/main/java/org/dnacronym/hygene/persistence/FileDatabase.java
+++ src/main/java/org/dnacronym/hygene/persistence/FileDatabase.java
@@ -17,7 +17,7 @@ import java.sql.SQLException;
 public final class FileDatabase implements AutoCloseable {
     private static final Logger LOGGER = LogManager.getLogger(FileDatabase.class);
 
-    static final int DB_VERSION = 8;
+    static final int DB_VERSION = 9;
 
     private final String fileName;
     private FileDatabaseDriver fileDatabaseDriver;
