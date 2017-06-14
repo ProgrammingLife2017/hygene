5aca24d3e82e70a5d97cd3b78463f9d45ef43e2d
Joel Abrahams <abrahamsjo@gmail.com>
2017-06-15 00:44:55 +0200

    [ci skip] Create simple GraphAnnotationVisualizer
    It's pretty ugly, but its there.

--- src/main/java/org/dnacronym/hygene/ui/graph/GraphAnnotationVisualizer.java
+++ src/main/java/org/dnacronym/hygene/ui/graph/GraphAnnotationVisualizer.java
@@ -1,13 +1,18 @@
 package org.dnacronym.hygene.ui.graph;
 
 import javafx.scene.canvas.GraphicsContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.dnacronym.hygene.coordinatesystem.GenomeIndex;
 import org.dnacronym.hygene.coordinatesystem.GenomePoint;
 import org.dnacronym.hygene.graph.NewNode;
 import org.dnacronym.hygene.graph.Segment;
 import org.dnacronym.hygene.models.FeatureAnnotation;
+import org.dnacronym.hygene.parser.GfaFile;
 import org.dnacronym.hygene.parser.GffFile;
+import org.dnacronym.hygene.persistence.FileDatabase;
 
+import java.io.IOException;
 import java.sql.SQLException;
 import java.util.Collection;
 
@@ -15,23 +20,37 @@ import java.util.Collection;
 /**
  *
  */
+@SuppressWarnings("PMD")
 public final class GraphAnnotationVisualizer {
+    private static final Logger LOGGER = LogManager.getLogger(GraphAnnotationVisualizer.class);
     private static final int ANNOTATION_MARKER_HEIGHT = 10;
 
     private final GraphStore graphStore;
     private final GraphDimensionsCalculator graphDimensionsCalculator;
-    private final GenomeIndex genomeIndex;
+    private GenomeIndex genomeIndex;
 
     private GraphicsContext graphicsContext;
     private double canvasWidth;
 
 
+    /**
+     *
+     * @param graphStore
+     * @param graphDimensionsCalculator
+     */
     public GraphAnnotationVisualizer(final GraphStore graphStore,
-                                     final GraphDimensionsCalculator graphDimensionsCalculator,
-                                     final GenomeIndex genomeIndex) {
+                                     final GraphDimensionsCalculator graphDimensionsCalculator) {
         this.graphStore = graphStore;
         this.graphDimensionsCalculator = graphDimensionsCalculator;
-        this.genomeIndex = genomeIndex;
+
+        final GfaFile gfaFile = graphStore.getGfaFileProperty().get();
+        try {
+            genomeIndex = new GenomeIndex(gfaFile, new FileDatabase(gfaFile.getFileName()));
+        } catch (final SQLException e) {
+            LOGGER.error(e);
+        } catch (final IOException e) {
+            LOGGER.error(e);
+        }
     }
 
 
@@ -45,7 +64,7 @@ public final class GraphAnnotationVisualizer {
     /**
      * @param canvasWidth
      */
-    public void setCanvasWidth(final int canvasWidth) {
+    public void setCanvasWidth(final double canvasWidth) {
         this.canvasWidth = canvasWidth;
     }
 
@@ -53,7 +72,11 @@ public final class GraphAnnotationVisualizer {
      * @param nodes
      * @throws SQLException
      */
-    public void draw(final Collection<NewNode> nodes) throws SQLException {
+    public void draw(final Collection<NewNode> nodes) {
+        if (genomeIndex == null || graphicsContext == null) {
+            return;
+        }
+
         final GenomePoint[] genomePoints = new GenomePoint[2];
 
         for (final GffFile gffFile : graphStore.getGffFiles()) {
@@ -64,38 +87,42 @@ public final class GraphAnnotationVisualizer {
             final int endOffset = featureAnnotation.getSubFeatureAnnotations().get(0).getEnd();
 
             final boolean[] present = {false};
-            genomeIndex.getGenomePoint(genome, startOffset)
-                    .ifPresent(genomePointStart -> {
-                        genomePoints[0] = genomePointStart;
-                        present[0] = true;
-                    });
-            genomeIndex.getGenomePoint(genome, endOffset)
-                    .ifPresent(genomePointEnd -> {
-                        genomePoints[1] = genomePointEnd;
-                        present[0] = true;
-                    });
-            if (!present[0]) {
-                continue;
-            }
+            try {
+                genomeIndex.getGenomePoint(genome, startOffset)
+                        .ifPresent(genomePointStart -> {
+                            genomePoints[0] = genomePointStart;
+                            present[0] = true;
+                        });
+                genomeIndex.getGenomePoint(genome, endOffset)
+                        .ifPresent(genomePointEnd -> {
+                            genomePoints[1] = genomePointEnd;
+                            present[0] = true;
+                        });
+                if (!present[0]) {
+                    continue;
+                }
 
-            final GenomePoint genomePointStart = genomePoints[0];
-            final GenomePoint genomePointEnd = genomePoints[1];
+                final GenomePoint genomePointStart = genomePoints[0];
+                final GenomePoint genomePointEnd = genomePoints[1];
 
-            final int startNodeId = genomePointStart.getNodeId();
-            final int endNodeId = genomePointEnd.getNodeId();
+                final int startNodeId = genomePointStart.getNodeId();
+                final int endNodeId = genomePointEnd.getNodeId();
 
-            final Segment[] filteredNodes = (Segment[]) nodes.stream().filter(node ->
-                    node instanceof Segment
-                            && (((Segment) node).getId() == startNodeId
-                            || ((Segment) node).getId() == endNodeId)).toArray();
+                final Segment[] filteredNodes = (Segment[]) nodes.stream().filter(node ->
+                        node instanceof Segment
+                                && (((Segment) node).getId() == startNodeId
+                                || ((Segment) node).getId() == endNodeId)).toArray();
 
-            if (filteredNodes.length == 1 && filteredNodes[0].getId() == startNodeId) {
-                drawFromStart(filteredNodes[0], genomePointStart.getBaseOffsetInNode());
-            } else if (filteredNodes.length == 1) {
-                drawTillEnd(filteredNodes[0], genomePointEnd.getBaseOffsetInNode());
-            } else if (filteredNodes.length == 2 && filteredNodes[0].getId() == endNodeId) {
-                drawWhole(filteredNodes[0], genomePointStart.getBaseOffsetInNode(),
-                        filteredNodes[1], genomePointEnd.getBaseOffsetInNode());
+                if (filteredNodes.length == 1 && filteredNodes[0].getId() == startNodeId) {
+                    drawFromStart(filteredNodes[0], genomePointStart.getBaseOffsetInNode());
+                } else if (filteredNodes.length == 1) {
+                    drawTillEnd(filteredNodes[0], genomePointEnd.getBaseOffsetInNode());
+                } else if (filteredNodes.length == 2 && filteredNodes[0].getId() == endNodeId) {
+                    drawWhole(filteredNodes[0], genomePointStart.getBaseOffsetInNode(),
+                            filteredNodes[1], genomePointEnd.getBaseOffsetInNode());
+                }
+            } catch (final SQLException e) {
+                LOGGER.error("Unable to get genome point of '" + genome + "'.", e);
             }
         }
     }
