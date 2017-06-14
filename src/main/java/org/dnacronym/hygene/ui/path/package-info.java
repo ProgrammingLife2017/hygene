d39313947cbeaa6936045ab7326060b5b2312c20
Niels de Bruin <nielsdebruin@gmail.com>
2017-06-14 22:44:38 +0200

    Add additional tests for edge and package.info

---
+++ src/main/java/org/dnacronym/hygene/ui/path/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Classes that deal with displaying the path of a genome.
+ */
+package org.dnacronym.hygene.ui.path;
--- src/test/java/org/dnacronym/hygene/graph/EdgeTest.java
+++ src/test/java/org/dnacronym/hygene/graph/EdgeTest.java
@@ -3,6 +3,10 @@ package org.dnacronym.hygene.graph;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 
@@ -14,12 +18,14 @@ abstract class EdgeTest {
     private Edge edge;
     private NewNode from;
     private NewNode to;
+    private Set<String> genomes;
 
 
     @BeforeEach
     void setUp() {
         from = mock(NewNode.class);
         to = mock(NewNode.class);
+        genomes = new HashSet<>(Arrays.asList("a", "b", "c"));
     }
 
 
@@ -31,6 +37,30 @@ abstract class EdgeTest {
     @Test
     final void testGetTo() {
         assertThat(edge.getTo()).isEqualTo(to);
+    }
+
+    @Test
+    void testGetSetGenomes() {
+        edge.setGenomes(genomes);
+        assertThat(edge.getGenomes()).isEqualTo(genomes);
+    }
+
+    @Test
+    void testGetImportance() {
+        edge.setGenomes(genomes);
+        assertThat(edge.getImportance()).isEqualTo(genomes.size());
+    }
+
+    @Test
+    void testGetImportantNullCase() {
+        edge.setGenomes(null);
+        assertThat(edge.getImportance()).isEqualTo(1);
+    }
+
+    @Test
+    void testGetImportantEmptyCase() {
+        edge.setGenomes(new HashSet<>());
+        assertThat(edge.getImportance()).isEqualTo(1);
     }
 
 
