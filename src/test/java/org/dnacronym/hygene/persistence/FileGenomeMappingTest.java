d7ad62d447818387a495e6b3f95a0c0d0ece0f2e
Niels de Bruin <nielsdebruin@gmail.com>
2017-06-15 15:26:06 +0200

    Add unit test for full coverage on FileGenomeMappings

---
+++ src/test/java/org/dnacronym/hygene/persistence/FileGenomeMappingTest.java
@@ -0,0 +1,84 @@
+package org.dnacronym.hygene.persistence;
+
+import org.dnacronym.hygene.parser.GfaFile;
+import org.dnacronym.hygene.parser.ParseException;
+import org.dnacronym.hygene.parser.ProgressUpdater;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
+
+
+/**
+ * Unit test for {@link FileGenomeMapping}
+ */
+class FileGenomeMappingTest extends FileDatabaseTestBase {
+    private FileDatabase fileDatabase;
+    private FileGenomeMapping fileGenomeMapping;
+
+    @BeforeEach
+    void setUp() throws IOException, SQLException {
+        super.setUp();
+        fileDatabase = new FileDatabase(GFA_FILE_NAME);
+        fileGenomeMapping = new FileGenomeMapping(fileDatabase);
+    }
+
+    @AfterEach
+    void tearDown() throws IOException, SQLException {
+        fileDatabase.close();
+        super.tearDown();
+    }
+
+    @Test
+    void testGetMappings() throws SQLException {
+        assertThat(fileGenomeMapping.getMappings().size()).isEqualTo(0);
+    }
+
+    @Test
+    void testRestoreGraph() throws SQLException, ParseException {
+        new GfaFile(GFA_FILE_NAME).parse(mock(ProgressUpdater.class));
+        assertThat(fileGenomeMapping.getMappings().size()).isEqualTo(2);
+    }
+
+    @Test
+    void testAddMappings1() throws SQLException {
+        fileGenomeMapping.addMapping("1", "sample.fasta");
+        assertThat(fileGenomeMapping.getMappings().size()).isEqualTo(1);
+        assertThat(fileGenomeMapping.getMappings().get("1")).isEqualTo("sample.fasta");
+    }
+
+    @Test
+    void testAddMappings2() throws SQLException {
+        final Map<String, String> mappings = new HashMap<>();
+        mappings.put("1", "first.fasta");
+        mappings.put("2", "second.fasta");
+        fileGenomeMapping.addMapping(mappings);
+        assertThat(fileGenomeMapping.getMappings().size()).isEqualTo(2);
+        assertThat(fileGenomeMapping.getMappings().get("1")).isEqualTo("first.fasta");
+        assertThat(fileGenomeMapping.getMappings().get("2")).isEqualTo("second.fasta");
+    }
+
+    @Test
+    void testAddMappingsException() throws SQLException {
+        fileDatabase.close();
+        assertThrows(SQLException.class, () -> {
+            fileGenomeMapping.addMapping("1", "sample.fasta");
+        });
+    }
+
+    @Test
+    void testGetMappingsException() throws SQLException {
+        fileDatabase.close();
+        assertThrows(SQLException.class, () -> {
+            assertThat(fileGenomeMapping.getMappings().size()).isEqualTo(0);
+        });
+    }
+}
