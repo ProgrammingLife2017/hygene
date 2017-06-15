3221ac7e092cf9a6351d2cc5669690879edd34a8
Felix Dekker <felix@fwdekker.com>
2017-06-15 10:47:23 +0200

    Use method reference instead of lambda

--- src/main/java/org/dnacronym/hygene/graph/layout/FafospLayerer.java
+++ src/main/java/org/dnacronym/hygene/graph/layout/FafospLayerer.java
@@ -278,7 +278,7 @@ public final class FafospLayerer implements SugiyamaLayerer {
             this.nodes = subgraph.getNodes();
 
             final Long minPosition = nodes.stream()
-                    .map(node -> node.getXPosition())
+                    .map(NewNode::getXPosition)
                     .min(Long::compare)
                     .orElseThrow(() -> new IllegalStateException("Non-empty collection has no minimum."));
             this.minLayer = (int) ((minPosition + LAYER_WIDTH - 1) / LAYER_WIDTH);
