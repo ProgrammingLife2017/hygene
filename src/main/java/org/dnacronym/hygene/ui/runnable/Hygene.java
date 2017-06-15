6eef4935881e22b141ce9d77c80e2ab8dcefea10
Joel Abrahams <abrahamsjo@gmail.com>
2017-06-15 11:04:41 +0200

    Make thread Daemon thread

--- src/main/java/org/dnacronym/hygene/ui/runnable/Hygene.java
+++ src/main/java/org/dnacronym/hygene/ui/runnable/Hygene.java
@@ -108,7 +108,6 @@ public final class Hygene extends Application {
         graphMovementCalculator = new GraphMovementCalculator(graphDimensionsCalculator);
         HygeneEventBus.getInstance().register(graphDimensionsCalculator);
 
-
         sequenceVisualizer = new SequenceVisualizer();
         statusBar = new StatusBar();
 
