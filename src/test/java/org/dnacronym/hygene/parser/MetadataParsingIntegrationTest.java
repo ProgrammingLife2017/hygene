28a841d7162e6c75c1f76aef6c2cdfb09c09f841
Casper Boone <mail@casperboone.nl>
2017-06-17 21:43:22 +0200

    Fix line lengths

--- src/test/java/org/dnacronym/hygene/parser/MetadataParsingIntegrationTest.java
+++ src/test/java/org/dnacronym/hygene/parser/MetadataParsingIntegrationTest.java
@@ -10,7 +10,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 
 /**
- * Tests the integration between {@link MetadataParser}, {@link GfaFile}, {@link ArrayBasedNode} and {@link ArrayBasedEdge}.
+ * Tests the integration between {@link MetadataParser}, {@link GfaFile}, {@link ArrayBasedNode} and
+ * {@link ArrayBasedEdge}.
  */
 final class MetadataParsingIntegrationTest {
     @Test
@@ -30,7 +31,8 @@ final class MetadataParsingIntegrationTest {
         final GfaFile gfaFile = new GfaFile("src/test/resources/gfa/simple.gfa");
         gfaFile.parse(ProgressUpdater.DUMMY);
 
-        final ArrayBasedEdge edge = ArrayBasedNode.fromGraph(gfaFile.getGraph(), 1).getOutgoingEdges().iterator().next();
+        final ArrayBasedNode node = ArrayBasedNode.fromGraph(gfaFile.getGraph(), 1);
+        final ArrayBasedEdge edge = node.getOutgoingEdges().iterator().next();
         final EdgeMetadata edgeMetadata = gfaFile.parseEdgeMetadata(edge.getByteOffset());
 
         assertThat(edgeMetadata.getFromOrient()).isEqualTo("+");
