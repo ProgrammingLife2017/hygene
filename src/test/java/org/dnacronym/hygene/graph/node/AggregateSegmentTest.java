211500631a5a4bda78f8d3d56527f40080a79e45
Felix Dekker <felix@fwdekker.com>
2017-06-21 19:52:44 +0200

    Test new AggregateSegment functions

--- src/test/java/org/dnacronym/hygene/graph/node/AggregateSegmentTest.java
+++ src/test/java/org/dnacronym/hygene/graph/node/AggregateSegmentTest.java
@@ -1,5 +1,6 @@
 package org.dnacronym.hygene.graph.node;
 
+import org.dnacronym.hygene.graph.metadata.NodeMetadata;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
@@ -7,6 +8,7 @@ import java.util.Collection;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
@@ -99,5 +101,76 @@ class AggregateSegmentTest {
         final AggregateSegment aggregateSegment = new AggregateSegment(segments);
 
         assertThat(aggregateSegment.containsSegment(30)).isFalse();
+    }
+
+    @Test
+    void testGetSegmentTrue() {
+        final List<Segment> segments = new ArrayList<>();
+        final Segment segmentA = new Segment(14, 38, 17);
+        final Segment segmentB = new Segment(74, 86, 19);
+        segments.add(segmentA);
+        segments.add(segmentB);
+        final AggregateSegment aggregateSegment = new AggregateSegment(segments);
+
+        assertThat(aggregateSegment.getSegment(14))
+                .isNotEmpty()
+                .contains(segmentA);
+    }
+
+    @Test
+    void testGetSegmentFalse() {
+        final List<Segment> segments = new ArrayList<>();
+        final Segment segmentA = new Segment(79, 30, 68);
+        final Segment segmentB = new Segment(81, 7, 87);
+        segments.add(segmentA);
+        segments.add(segmentB);
+        final AggregateSegment aggregateSegment = new AggregateSegment(segments);
+
+        assertThat(aggregateSegment.getSegment(10)).isEmpty();
+    }
+
+    @Test
+    void testGetSegmentIds() {
+        final List<Segment> segments = new ArrayList<>();
+        final Segment segmentA = new Segment(58, 10, 87);
+        final Segment segmentB = new Segment(23, 98, 69);
+        segments.add(segmentA);
+        segments.add(segmentB);
+        final AggregateSegment aggregateSegment = new AggregateSegment(segments);
+
+        assertThat(aggregateSegment.getSegmentIds()).containsExactlyInAnyOrder(23, 58);
+    }
+
+    @Test
+    void testGetMetadataException() {
+        final Segment segment = new Segment(60, 91, 34);
+
+        assertThatThrownBy(() -> segment.getMetadata()).isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void testGetMetadata() {
+        final Segment segment = new Segment(89, 17, 23);
+        final NodeMetadata metadata = new NodeMetadata("name", "sequence", new ArrayList<>());
+
+        segment.setMetadata(metadata);
+
+        assertThat(segment.getMetadata()).isEqualTo(metadata);
+    }
+
+    @Test
+    void testHasMetadataDefault() {
+        final Segment segment = new Segment(68, 19, 60);
+
+        assertThat(segment.hasMetadata()).isFalse();
+    }
+
+    @Test
+    void testHasMetadataTrue() {
+        final Segment segment = new Segment(34, 78, 92);
+
+        segment.setMetadata(new NodeMetadata("name", "sequence", new ArrayList<>()));
+
+        assertThat(segment.hasMetadata()).isTrue();
     }
 }
