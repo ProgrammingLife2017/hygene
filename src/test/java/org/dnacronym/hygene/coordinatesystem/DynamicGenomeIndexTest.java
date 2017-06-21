961d400271c543bb3aad44cb66a3a8593e9b49ef
Georgios Andreadis <g.andreadis@student.tudelft.nl>
2017-06-21 22:31:17 +0200

    [ci skip] Add tests for DynamicGenomeIndex

--- src/test/java/org/dnacronym/hygene/coordinatesystem/DynamicGenomeIndexTest.java
+++ src/test/java/org/dnacronym/hygene/coordinatesystem/DynamicGenomeIndexTest.java
@@ -9,16 +9,19 @@ import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.sql.SQLException;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
+
 
 /**
  * Unit tests for {@link DynamicGenomeIndex}.
  */
 class DynamicGenomeIndexTest {
-    private static final String TEST_GFA_FILE_NAME = "C:\\Users\\gandr\\Downloads\\GFA Files\\chr19.hg38.w115.gfa";
+    private static final String TEST_GFA_FILE_NAME = "src/test/resources/gfa/dynamic-index.gfa";
 
     private DynamicGenomeIndex dynamicGenomeIndex;
     private GfaFile gfaFile;
+
 
     @BeforeEach
     void setUp() throws IOException, SQLException, GfaParseException {
@@ -26,10 +29,37 @@ class DynamicGenomeIndexTest {
         gfaFile.parse(mock(ProgressUpdater.class));
     }
 
+
     @Test
-    void testStuff() {
-        dynamicGenomeIndex = new DynamicGenomeIndex(gfaFile, "chr19.fa");
-        System.out.println("BUILDING INDEX");
+    void testInsertion() {
+        dynamicGenomeIndex = new DynamicGenomeIndex(gfaFile, "g1.fasta");
         dynamicGenomeIndex.buildIndex();
+
+        assertThat(dynamicGenomeIndex.getNodeByBase(2)).isEqualTo(1);
+        assertThat(dynamicGenomeIndex.getNodeByBase(3)).isEqualTo(2);
+        assertThat(dynamicGenomeIndex.getNodeByBase(5)).isEqualTo(2);
+        assertThat(dynamicGenomeIndex.getNodeByBase(7)).isEqualTo(3);
+    }
+
+    @Test
+    void testSecondInsertion() {
+        dynamicGenomeIndex = new DynamicGenomeIndex(gfaFile, "g2.fasta");
+        dynamicGenomeIndex.buildIndex();
+
+        assertThat(dynamicGenomeIndex.getNodeByBase(2)).isEqualTo(1);
+        assertThat(dynamicGenomeIndex.getNodeByBase(3)).isEqualTo(3);
+        assertThat(dynamicGenomeIndex.getNodeByBase(6)).isEqualTo(4);
+        assertThat(dynamicGenomeIndex.getNodeByBase(7)).isEqualTo(4);
+        assertThat(dynamicGenomeIndex.getNodeByBase(9)).isEqualTo(5);
+    }
+
+    @Test
+    void testNestedInsertion() {
+        dynamicGenomeIndex = new DynamicGenomeIndex(gfaFile, "g3.fasta");
+        dynamicGenomeIndex.buildIndex();
+
+        assertThat(dynamicGenomeIndex.getNodeByBase(2)).isEqualTo(1);
+        assertThat(dynamicGenomeIndex.getNodeByBase(3)).isEqualTo(6);
+        assertThat(dynamicGenomeIndex.getNodeByBase(5)).isEqualTo(6);
     }
 }
