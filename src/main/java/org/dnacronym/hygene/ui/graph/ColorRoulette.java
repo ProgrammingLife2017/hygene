31572134410a50970c27f0eb4b4f15b33f4f71aa
Niels de Bruin <nielsdebruin@gmail.com>
2017-06-21 13:21:59 +0200

    Cleanup ColorRoulette

--- src/main/java/org/dnacronym/hygene/ui/graph/ColorRoulette.java
+++ src/main/java/org/dnacronym/hygene/ui/graph/ColorRoulette.java
@@ -2,31 +2,26 @@ package org.dnacronym.hygene.ui.graph;
 
 import javafx.scene.paint.Color;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * Class that will sequentially generate a quantitative color scheme.
  */
 public class ColorRoulette {
-    private List<Color> colors;
-    int index = 0;
-
-    /**
-     * Construct a new Roulette
-     */
-    public ColorRoulette() {
-        colors = new ArrayList<>();
-    }
+    private int index = -1;
 
     /**
      * Retrieve the next color.
      */
     public synchronized Color getNext() {
-        return QuantitativeColors.values()[index].color;
+        DarkQuantitativeColors[] colors = DarkQuantitativeColors.values();
+        index = index++ % colors.length;
+
+        return colors[index].color;
     }
 
-    enum QuantitativeColors {
+    /**
+     * Enum representing a color scheme containing a dark quantitative color scheme.
+     */
+    enum DarkQuantitativeColors {
         LIGHT_BLUE(Color.color(166, 206, 227)),
         DARK_BLUE(Color.color(31, 120, 180)),
         LIGHT_GREEN(Color.color(178, 223, 138)),
@@ -34,7 +29,7 @@ public class ColorRoulette {
         PINK(Color.color(251, 154, 153)),
         RED(Color.color(227, 26, 28)),
         UNHEALTHLY_DIARREA(Color.color(253, 191, 111)),
-        ORANGE(Color.color(255, , 127, 0)),
+        ORANGE(Color.color(255, 127, 0)),
         VIOLET(Color.color(202, 178, 214)),
         PURPLE(Color.color(106, 61, 154)),
         YELLOW(Color.color(255, 255, 153)),
@@ -44,11 +39,11 @@ public class ColorRoulette {
 
 
         /**
-         * Construct a new {@link QuantitativeColors} instance.
+         * Construct a new {@link DarkQuantitativeColors} instance.
          *
          * @param color the color
          */
-        QuantitativeColors(final Color color) {
+        DarkQuantitativeColors(final Color color) {
             this.color = color;
         }
     }
