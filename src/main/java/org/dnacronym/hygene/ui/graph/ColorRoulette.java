a18cf77731ae97f455f608c689e29ace27003be4
Niels de Bruin <nielsdebruin@gmail.com>
2017-06-21 14:17:03 +0200

    Add tests for ColorRoulette

--- src/main/java/org/dnacronym/hygene/ui/graph/ColorRoulette.java
+++ src/main/java/org/dnacronym/hygene/ui/graph/ColorRoulette.java
@@ -5,15 +5,17 @@ import javafx.scene.paint.Color;
 /**
  * Class that will sequentially generate a quantitative color scheme.
  */
-public class ColorRoulette {
+public final class ColorRoulette {
     private int index = -1;
 
     /**
-     * Retrieve the next color.
+     * Retrieves the next {@link Color}.
+     *
+     * @return the {@link Color}
      */
     public synchronized Color getNext() {
-        DarkQuantitativeColors[] colors = DarkQuantitativeColors.values();
-        index = index++ % colors.length;
+        final DarkQuantitativeColors[] colors = DarkQuantitativeColors.values();
+        index = ++index % colors.length;
 
         return colors[index].color;
     }
@@ -22,18 +24,18 @@ public class ColorRoulette {
      * Enum representing a color scheme containing a dark quantitative color scheme.
      */
     enum DarkQuantitativeColors {
-        LIGHT_BLUE(Color.color(166, 206, 227)),
-        DARK_BLUE(Color.color(31, 120, 180)),
-        LIGHT_GREEN(Color.color(178, 223, 138)),
-        DARK_GREEN(Color.color(51, 160, 44)),
-        PINK(Color.color(251, 154, 153)),
-        RED(Color.color(227, 26, 28)),
-        UNHEALTHLY_DIARREA(Color.color(253, 191, 111)),
-        ORANGE(Color.color(255, 127, 0)),
-        VIOLET(Color.color(202, 178, 214)),
-        PURPLE(Color.color(106, 61, 154)),
-        YELLOW(Color.color(255, 255, 153)),
-        BROWN(Color.color(177, 89, 40));
+        LIGHT_BLUE(Color.rgb(166, 206, 227)),
+        DARK_BLUE(Color.rgb(31, 120, 180)),
+        LIGHT_GREEN(Color.rgb(178, 223, 138)),
+        DARK_GREEN(Color.rgb(51, 160, 44)),
+        PINK(Color.rgb(251, 154, 153)),
+        RED(Color.rgb(227, 26, 28)),
+        UNHEALTHLY_DIARREA(Color.rgb(253, 191, 111)),
+        ORANGE(Color.rgb(255, 127, 0)),
+        VIOLET(Color.rgb(202, 178, 214)),
+        PURPLE(Color.rgb(106, 61, 154)),
+        YELLOW(Color.rgb(255, 255, 153)),
+        BROWN(Color.rgb(177, 89, 40));
 
         private Color color;
 
@@ -45,6 +47,16 @@ public class ColorRoulette {
          */
         DarkQuantitativeColors(final Color color) {
             this.color = color;
+        }
+
+
+        /**
+         * Gets the {@link Color}.
+         *
+         * @return the {@link Color}
+         */
+        public Color getColor() {
+            return color;
         }
     }
 }
---
+++ src/test/java/org/dnacronym/hygene/ui/graph/ColorRouletteTest.java
@@ -0,0 +1,29 @@
+package org.dnacronym.hygene.ui.graph;
+
+import javafx.scene.paint.Color;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+/**
+ * Unit test for {@link ColorRoulette}.
+ */
+class ColorRouletteTest {
+    private ColorRoulette colorRoulette;
+
+    @BeforeEach
+    void setUp() throws IOException, SQLException {
+        colorRoulette = new ColorRoulette();
+    }
+
+    @Test
+    void getNext() {
+        final Color color = colorRoulette.getNext();
+
+        assertThat(color).isEqualTo(ColorRoulette.DarkQuantitativeColors.LIGHT_BLUE.getColor());
+    }
+}
