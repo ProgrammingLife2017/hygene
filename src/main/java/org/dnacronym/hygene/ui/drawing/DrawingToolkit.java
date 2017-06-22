3d8abb41de38f80a0e198323048591b65b731a72
Joel Abrahams <abrahamsjo@gmail.com>
2017-06-22 18:46:08 +0200

    Add annotation drawing to GraphVisualizer

--- src/main/java/org/dnacronym/hygene/ui/drawing/DrawingToolkit.java
+++ src/main/java/org/dnacronym/hygene/ui/drawing/DrawingToolkit.java
@@ -16,7 +16,7 @@ public abstract class DrawingToolkit {
      */
     static final int ANNOTATION_DASH_DEFAULT = 0;
 
-    private double annotationHeight = 2;
+    private double annotationHeight = 1;
     private GraphicsContext graphicsContext;
 
 
