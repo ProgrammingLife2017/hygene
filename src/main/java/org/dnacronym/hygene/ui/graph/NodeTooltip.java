b4cc252cb5714331fe38577aaed089e1da6567f0
Joel Abrahams <abrahamsjo@gmail.com>
2017-06-28 12:40:43 +0200

    Show genomes names even if they were indexed in the GFA

--- src/main/java/org/dnacronym/hygene/ui/graph/NodeTooltip.java
+++ src/main/java/org/dnacronym/hygene/ui/graph/NodeTooltip.java
@@ -4,9 +4,12 @@ import javafx.collections.transformation.FilteredList;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.paint.Color;
+import org.apache.commons.lang3.StringUtils;
 import org.dnacronym.hygene.graph.node.GfaNode;
 import org.dnacronym.hygene.graph.node.Segment;
 import org.dnacronym.hygene.ui.path.GenomePath;
+
+import javax.inject.Inject;
 
 
 /**
@@ -32,6 +35,8 @@ public class NodeTooltip {
 
     private int offset;
 
+    @Inject
+    private GraphStore graphStore;
 
     /**
      * Constructs {@link NodeTooltip}.
@@ -176,14 +181,17 @@ public class NodeTooltip {
                 .subList(0, genomeCount)
                 .forEach(genome -> {
                     final FilteredList<GenomePath> possiblePaths = graphVisualizer.getGenomePathsProperty()
-                            .filtered(genomePath -> genomePath.getName().equals(genome));
+                            .filtered(genomePath -> genomePath.getName().equals(genome)
+                                    || genomePath.getIndex().equals(genome));
                     if (possiblePaths.isEmpty()) {
                         return;
                     }
 
                     final Color color = possiblePaths.get(0).getColor().get();
                     graphicsContext.setFill(color == null ? Color.BLACK : color);
-                    graphicsContext.fillText("    " + genome,
+
+                    graphicsContext.fillText(
+                            "    " + (StringUtils.isNumeric(genome) ? possiblePaths.get(0).getName() : genome),
                             middleX - (DEFAULT_WIDTH / 2) + X_PADDING,
                             belowY + LINE_HEIGHT + Y_PADDING + offset);
                     offset += LINE_HEIGHT;
